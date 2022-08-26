90865a229b033d867ddd573f5898964295c75716
Pedro Nauck <pedronauck@gmail.com>
2022-08-25 21:21:04 -0700

    chore(app): pr changes

--- packages/app/src/types.ts
+++ packages/app/src/types.ts
@@ -2,9 +2,6 @@ export enum Pages {
   'home' = '/',
 }
 
-/**
- * This should be generated automatic with Codegen and GraphQL endpoints
- */
 export type Account = {
   name: string;
   address: string;
