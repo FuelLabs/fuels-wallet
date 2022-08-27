a7f99120a75a9451597036eae479783e78b80c65
Pedro Nauck <pedronauck@gmail.com>
2022-08-26 20:15:59 -0700

    feat(app): add MnemonicRead component of sign-up

---
+++ packages/app/src/systems/SignUp/components/MnemonicRead/MnemonicRead.stories.tsx
@@ -0,0 +1,37 @@
+import { BoxCentered } from "@fuel-ui/react";
+import { action } from "@storybook/addon-actions";
+
+import { MnemonicRead } from "./MnemonicRead";
+
+export default {
+  component: MnemonicRead,
+  title: "SignUp/Components/MnemonicRead",
+  parameters: {
+    layout: "fullscreen",
+  },
+};
+
+const WORDS = [
+  "strange",
+  "purple",
+  "adamant",
+  "crayons",
+  "entice",
+  "fun",
+  "eloquent",
+  "missiles",
+  "milk",
+  "ice",
+  "cream",
+  "apple",
+];
+
+export const Usage = () => (
+  <BoxCentered minHS>
+    <MnemonicRead
+      words={WORDS}
+      onNext={action("onNext")}
+      onCancel={action("onCancel")}
+    />
+  </BoxCentered>
+);
