d6507cc64db34aafac178395fd8b11e45820900d
Pedro Nauck <pedronauck@gmail.com>
2022-08-26 17:07:56 -0700

    chore(app): remove ethers library

--- packages/app/src/systems/Core/utils/math.tsx
+++ packages/app/src/systems/Core/utils/math.tsx
@@ -1,35 +1,25 @@
-import * as ethers from "ethers";
 import type { BigNumberish } from "fuels";
 import { toBigInt } from "fuels";
 
-import { DECIMAL_UNITS, FIXED_UNITS } from "~/config";
+import { DECIMAL_UNITS } from "~/config";
 import type { Maybe } from "~/systems/Core";
-
-export function toFixed(
-  number: Maybe<BigNumberish>,
-  maxDecimals: number = FIXED_UNITS
-) {
-  const [amount, decimals = "0"] = String(number?.toString() || "0.0").split(
-    "."
-  );
-  const minDecimals = decimals.split("").findIndex((u: string) => u !== "0");
-  const canShowMinDecimals = minDecimals >= maxDecimals && amount === "0";
-  const decimalFormatted = decimals.slice(
-    0,
-    canShowMinDecimals ? minDecimals + 1 : maxDecimals
-  );
-  return [amount || 0, ".", ...decimalFormatted].join("");
-}
-
-export function formatUnits(
-  number: BigNumberish,
-  precision: number = DECIMAL_UNITS
-): string {
-  return ethers.utils.formatUnits(number, precision);
-}
 
 export function safeBigInt(value?: Maybe<bigint>, defaultValue?: number) {
   return value || toBigInt(defaultValue || 0);
+}
+
+export function parseUnits(
+  value: string | BigNumberish,
+  precision: number = DECIMAL_UNITS
+) {
+  return toBigInt(parseFloat(value.toString()) * 10 ** precision);
+}
+
+export function formatUnits(
+  value: string | BigNumberish,
+  precision: number = DECIMAL_UNITS
+) {
+  return parseFloat(value.toString()) / 10 ** precision;
 }
 
 export function parseAndFormat(
@@ -40,7 +30,8 @@ export function parseAndFormat(
   if (typeof value === "number") {
     val = BigInt(Math.trunc(value));
   }
-  return ethers.utils.commify(
-    toFixed(formatUnits(val, precision), FIXED_UNITS)
-  );
+  return new Intl.NumberFormat("es", {
+    // style: "unit",
+    minimumFractionDigits: 3,
+  }).format(formatUnits(val, precision));
 }
