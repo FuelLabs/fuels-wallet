9a977b34bc2ee80f8168eafc943b705bf7cef6d6
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2022-08-29 20:59:52 -0300

    refact: change style pattern

--- packages/app/src/systems/Account/components/BalanceWidget/BalanceWidget.tsx
+++ packages/app/src/systems/Account/components/BalanceWidget/BalanceWidget.tsx
@@ -6,64 +6,18 @@ import type { Account } from "../../types";
 import { BalanceWidgetLoader } from "./BalanceWidgetLoader";
 
 import { shortAddress } from "~/systems/Core";
+import { cssObj } from "@fuel-ui/css";
 
 type BalanceWidgetWrapperProps = {
   children: ReactNode;
 };
 
 export function BalanceWidgetWrapper({ children }: BalanceWidgetWrapperProps) {
-  const backgroundProps = {
-    position: "absolute",
-    width: "100%",
-    height: "100%",
-    boxShadow: "2px 0px 4px rgba(0, 0, 0, 0.15)",
-    transform: "skew(-25deg)",
-    borderTopRightRadius: "40px 35px",
-    borderBottomRightRadius: "35px 25px",
-    borderBottomLeftRadius: "40px 50px",
-    borderTopLeftRadius: "10px",
-  };
-
   return (
-    <Box
-      css={{
-        minHeight: 88,
-        position: "relative",
-      }}
-    >
-      <Box
-        css={{
-          ...backgroundProps,
-          background:
-            "linear-gradient(180deg, #0F0F0F -9.3%, rgba(18, 20, 20, 0.45) 35.67%)",
-          top: 10,
-          left: 7,
-        }}
-      >
-        &nbsp;
-      </Box>
-      <Box
-        css={{
-          ...backgroundProps,
-          background:
-            "linear-gradient(180deg, #1F1F1F 0%, rgba(28, 30, 31, 0.45) 41.15%)",
-          top: 3,
-          left: 3,
-        }}
-      >
-        &nbsp;
-      </Box>
-      <Flex
-        css={{
-          position: "absolute",
-          width: "100%",
-          height: "100%",
-          top: 0,
-          left: 0,
-        }}
-      >
-        {children}
-      </Flex>
+    <Box css={styles.balanceWidgetWrapper}>
+      <Box css={styles.backgroundShadow}>&nbsp;</Box>
+      <Box css={styles.backgroundFront}>&nbsp;</Box>
+      <Flex css={styles.contentWrapper}>{children}</Flex>
     </Box>
   );
 }
@@ -95,35 +49,13 @@ export function BalanceWidget({ account, isHidden }: BalanceWidgetProps) {
           color="gray"
           icon={<Icon icon="CaretDown" color="gray8" />}
           aria-label="Expand"
-          css={{
-            marginTop: 8,
-            height: "20px !important",
-            padding: "0 3px !important",
-            borderRadius: 8,
-          }}
+          css={styles.caretDownIcon}
         />
       </Flex>
-      <Flex
-        justify="space-between"
-        css={{
-          flex: "1 0",
-        }}
-      >
-        <Flex
-          direction="column"
-          css={{
-            marginLeft: 14,
-            alignSelf: "center",
-          }}
-        >
+      <Flex justify="space-between" css={{ flex: "1 0" }}>
+        <Flex direction="column" css={{ marginLeft: 14, alignSelf: "center" }}>
           <Flex align="center">
-            <Text
-              fontSize="sm"
-              color="gray11"
-              css={{
-                fontWeight: "bold",
-              }}
-            >
+            <Text fontSize="sm" color="gray11" css={{ fontWeight: "bold" }}>
               {shortAddress(account.address)}
             </Text>
             <IconButton
@@ -133,27 +65,18 @@ export function BalanceWidget({ account, isHidden }: BalanceWidgetProps) {
               color="gray"
               icon={<Icon icon="Copy" color="gray8" size={18} weight="thin" />}
               aria-label="Copy address"
-              css={{
-                marginLeft: 4,
-              }}
+              css={{ marginLeft: 4 }}
             />
           </Flex>
           <Text
             color={isHidden ? "gray10" : "gray12"}
             fontSize="2xl"
-            css={{
-              fontWeight: "bold",
-            }}
+            css={{ fontWeight: "bold" }}
           >
             $&nbsp;{isHidden ? "•••••" : account.balance || "0.00"}
           </Text>
         </Flex>
-        <Box
-          css={{
-            marginRight: 6,
-            marginTop: 8,
-          }}
-        >
+        <Box css={{ marginRight: 6, marginTop: 8 }}>
           <IconButton
             size="xs"
             variant="link"
@@ -181,3 +104,49 @@ export function BalanceWidget({ account, isHidden }: BalanceWidgetProps) {
 }
 
 BalanceWidget.Loader = BalanceWidgetLoader;
+
+const backgroundCss = {
+  position: "absolute",
+  width: "100%",
+  height: "100%",
+  boxShadow: "2px 0px 4px rgba(0, 0, 0, 0.15)",
+  transform: "skew(-25deg)",
+  borderTopRightRadius: "40px 35px",
+  borderBottomRightRadius: "35px 25px",
+  borderBottomLeftRadius: "40px 50px",
+  borderTopLeftRadius: "10px",
+};
+
+const styles = {
+  backgroundFront: cssObj({
+    ...backgroundCss,
+    background:
+      "linear-gradient(180deg, #1F1F1F 0%, rgba(28, 30, 31, 0.45) 41.15%)",
+    top: 3,
+    left: 3,
+  }),
+  backgroundShadow: cssObj({
+    ...backgroundCss,
+    background:
+      "linear-gradient(180deg, #0F0F0F -9.3%, rgba(18, 20, 20, 0.45) 35.67%)",
+    top: 10,
+    left: 7,
+  }),
+  contentWrapper: cssObj({
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    top: 0,
+    left: 0,
+  }),
+  balanceWidgetWrapper: cssObj({
+    minHeight: 88,
+    position: "relative",
+  }),
+  caretDownIcon: cssObj({
+    marginTop: 8,
+    height: "20px !important",
+    padding: "0 3px !important",
+    borderRadius: 8,
+  }),
+};
