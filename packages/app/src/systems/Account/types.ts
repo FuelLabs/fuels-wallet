db36cb2114ab71da647b8c075560e6aab486b4d7
Pedro Nauck <pedronauck@gmail.com>
2022-08-30 00:35:24 -0700

    feat(app): create wallet manager service

--- packages/app/src/systems/Account/types.ts
+++ packages/app/src/systems/Account/types.ts
@@ -1,4 +1,10 @@
+export type Vault = {
+  key: string;
+  data: string;
+};
+
 export type Account = {
+  vaultKey?: string;
   name: string;
   address: string;
   isHidden?: boolean;
---
+++ packages/app/src/systems/Account/utils/storage.tsx
@@ -0,0 +1,19 @@
+import { db } from "~/systems/Core";
+
+export class IndexedDBStorage {
+  async getItem(key: string) {
+    return db.getVault(key);
+  }
+
+  async setItem(key: string, data: string) {
+    return db.addVault(key, data);
+  }
+
+  async removeItem(key: string) {
+    return db.removeVault(key);
+  }
+
+  async clear() {
+    return db.clearVaults();
+  }
+}
