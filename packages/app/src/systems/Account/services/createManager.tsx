ccb5bc154fae375b6994e5d2a2a6e79b83b13c0e
Pedro Nauck <pedronauck@gmail.com>
2022-08-30 00:35:24 -0700

    feat(app): create wallet manager service

---
+++ packages/app/src/systems/Account/services/createManager.tsx
@@ -0,0 +1,47 @@
+import { WalletManager } from "@fuel-ts/wallet-manager";
+import { Buffer as BufferPolyfill } from "buffer";
+
+import { IndexedDBStorage } from "../utils";
+
+import { db } from "~/systems/Core";
+
+/**
+ * TODO: this is here because @fuel-ts/wallet-manager is getting an error
+ * related to Buffer when trying to use manager.addVault()
+ */
+globalThis.Buffer = BufferPolyfill;
+
+type CreateManagerData = {
+  password?: string;
+  mnemonic?: string[];
+};
+
+export async function createManager(data: CreateManagerData) {
+  if (!data?.password || !data?.mnemonic) {
+    throw new Error("Invalid data");
+  }
+
+  /**
+   * TODO: check how we will handle with managers here
+   */
+  await db.clearVaults();
+
+  /**
+   * TODO: this is needed because of a typing error with StorageAbstract from fuels-ts
+   */
+  const storage = new IndexedDBStorage() as never;
+  const manager = new WalletManager({ storage });
+
+  try {
+    await manager.unlock(data.password);
+    await manager.addVault({
+      type: "mnemonic",
+      secret: data.mnemonic.join(" "),
+    });
+    return manager;
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+    throw error;
+  }
+}
