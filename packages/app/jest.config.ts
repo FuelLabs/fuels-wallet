df07d926558d5f6dc741dd39a4c1717c8956991e
Pedro Nauck <pedronauck@gmail.com>
2022-08-30 18:07:11 -0700

    chore: update @fuel-ui/test-utils

--- packages/app/jest.config.ts
+++ packages/app/jest.config.ts
@@ -1,31 +1,22 @@
+import { config as baseConfig } from '@fuel-ui/test-utils/config';
 import type { Config } from '@jest/types';
-import { resolve } from 'path';
 
 import './load.envs.js';
 import pkg from './package.json';
 
 const config: Config.InitialOptions = {
+  ...baseConfig,
   rootDir: __dirname,
   displayName: pkg.name,
-  preset: 'ts-jest/presets/default-esm',
-  globals: {
-    'ts-jest': {
-      useESM: true,
-    },
-  },
-  testTimeout: 20000,
-  testEnvironment: 'jsdom',
-  testMatch: ['<rootDir>/**/?(*.)+(spec|test).[jt]s?(x)'],
-  setupFiles: ['dotenv/config'],
-  setupFilesAfterEnv: [resolve(__dirname, './jest.setup.ts')],
-  testPathIgnorePatterns: ['/lib/', '/node_modules/'],
-  modulePathIgnorePatterns: ['/dist/'],
-  coveragePathIgnorePatterns: ['/dist/'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
-    '.+\\.(css|scss|png|jpg|svg)$': 'jest-transform-stub',
-    '~/(.*)$': '<rootDir>/src/$1',
-    '^(\\.{1,2}/.*)\\.js$': '$1',
+    ...baseConfig.moduleNameMapper,
+    '^dexie$': require.resolve('dexie'),
+    /**
+     * This is here because when using pnpm link with @fuel-ui packages,
+     * two versions of react can cause runtime errors
+     * */
+    '^react$': require.resolve('react'),
+    '^react-dom$': require.resolve('react-dom'),
   },
 };
 
