a01446c2e68a3c09d81695c2a72fc6b3228c8a32
Pedro Nauck <pedronauck@gmail.com>
2022-09-02 16:58:59 -0700

    fix: cypress at root

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,8 +7,8 @@ const PORT = NODE_ENV === 'test' ? 3001 : 3000;
 export default defineConfig({
   e2e: {
     baseUrl: `http://localhost:${PORT}`,
-    supportFile: './cypress/support/e2e.ts',
-    specPattern: './cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: './packages/app/cypress/support/e2e.ts',
+    specPattern: './packages/app/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
   viewportWidth: 1280,
   viewportHeight: 720,
