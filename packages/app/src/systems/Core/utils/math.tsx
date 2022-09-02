7383aa0ee194e9a953d6de7aa0840f4141f7b70f
Pedro Nauck <pedronauck@gmail.com>
2022-09-01 17:41:17 -0700

    chore: pr changes

--- packages/app/src/systems/Core/utils/math.tsx
+++ packages/app/src/systems/Core/utils/math.tsx
@@ -1,7 +1,12 @@
 import type { BigNumberish } from "fuels";
 import { toBigInt } from "fuels";
 
-import * as cfg from "~/config";
+import {
+  DECIMAL_UNITS,
+  MIN_FRACTION_DIGITS,
+  MAX_FRACTION_DIGITS,
+  FORMAT_LANGUAGE,
+} from "~/config";
 import type { Maybe } from "~/systems/Core";
 
 export function safeBigInt(value?: Maybe<BigNumberish>, defaultValue?: number) {
@@ -10,7 +15,7 @@ export function safeBigInt(value?: Maybe<BigNumberish>, defaultValue?: number) {
 
 export function unitsToAmount(
   value: Maybe<BigNumberish>,
-  precision: number = cfg.DECIMAL_UNITS
+  precision: number = DECIMAL_UNITS
 ) {
   const val = safeBigInt(value);
   return toBigInt(parseFloat(val.toString()) * 10 ** precision);
@@ -18,7 +23,7 @@ export function unitsToAmount(
 
 export function amountToUnits(
   value: Maybe<BigNumberish>,
-  precision: number = cfg.DECIMAL_UNITS
+  precision: number = DECIMAL_UNITS
 ) {
   const val = safeBigInt(value);
   return parseFloat(val.toString()) / 10 ** precision;
@@ -33,12 +38,12 @@ type FormatOpts = {
 
 export function formatUnits(value: Maybe<BigNumberish>, opts?: FormatOpts) {
   const val = typeof value === "number" ? BigInt(Math.trunc(value)) : value;
-  const precision = opts?.precision || cfg.DECIMAL_UNITS;
-  const minDigits = opts?.minDigits || cfg.MIN_FRACTION_DIGITS;
-  const maxDigits = opts?.maxDigits || cfg.MAX_FRACTION_DIGITS;
+  const precision = opts?.precision || DECIMAL_UNITS;
+  const minDigits = opts?.minDigits || MIN_FRACTION_DIGITS;
+  const maxDigits = opts?.maxDigits || MAX_FRACTION_DIGITS;
 
   const units = amountToUnits(safeBigInt(val), precision);
-  const formatted = new Intl.NumberFormat(cfg.FORMAT_LANGUAGE, {
+  const formatted = new Intl.NumberFormat(FORMAT_LANGUAGE, {
     minimumFractionDigits: minDigits,
     maximumFractionDigits: maxDigits,
   }).format(units);
