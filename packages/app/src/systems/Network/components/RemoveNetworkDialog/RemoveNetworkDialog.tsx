6e61a221cd1e9713155b9f7d7ff7916a7c250c4c
Pedro Nauck <pedronauck@gmail.com>
2022-09-06 20:01:30 -0700

    feat(react): add RemoveNetworkDialog component

---
+++ packages/app/src/systems/Network/components/RemoveNetworkDialog/RemoveNetworkDialog.tsx
@@ -0,0 +1,53 @@
+import { AlertDialog, Button } from "@fuel-ui/react";
+import type { ReactNode } from "react";
+import { useState } from "react";
+
+import type { Network } from "../../types";
+
+type RemoveNetworkDialogProps = {
+  children: ReactNode;
+  network: Network;
+  onConfirm: () => void;
+};
+
+export function RemoveNetworkDialog({
+  network,
+  onConfirm,
+  children,
+}: RemoveNetworkDialogProps) {
+  const [opened, setOpened] = useState(false);
+
+  function handleCancel() {
+    setOpened(false);
+  }
+
+  function handleConfirm() {
+    onConfirm();
+    setOpened(false);
+  }
+
+  return (
+    <AlertDialog open={opened} onOpenChange={setOpened}>
+      <AlertDialog.Trigger>{children}</AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Heading>Are you absolutely sure?</AlertDialog.Heading>
+        <AlertDialog.Description>
+          This action cannot be undone. This will permanently delete
+          {network.name}.
+        </AlertDialog.Description>
+        <AlertDialog.Footer>
+          <AlertDialog.Cancel>
+            <Button color="gray" variant="ghost" onPress={handleCancel}>
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="outlined" color="tomato" onPress={handleConfirm}>
+              Confirm
+            </Button>
+          </AlertDialog.Action>
+        </AlertDialog.Footer>
+      </AlertDialog.Content>
+    </AlertDialog>
+  );
+}
