33adcfdad1232845b80969b971976f4d158c5f2a
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2022-09-16 00:32:00 -0300

    chore

--- packages/app/src/mocks/server.ts
+++ packages/app/src/mocks/server.ts
@@ -4,14 +4,8 @@ import { setupServer } from 'msw/node';
 // This configures a request mocking server with the given request handlers.
 export const mockServer = (handlers: [RestHandler | GraphQLHandler]) => {
   const server = setupServer(...handlers);
-
-  // Establish API mocking before all tests.
   beforeAll(() => server.listen());
-  // Reset any request handlers that we may add during the tests,
-  // so they don't affect other tests.
   afterEach(() => server.resetHandlers());
-  // Clean up after the tests are finished.
   afterAll(() => server.close());
-
   return server;
 };
