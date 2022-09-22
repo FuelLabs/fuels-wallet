a9ae45a1a31225cb7e951d4a3334c9509256450b
Pedro Nauck <pedronauck@gmail.com>
2022-09-22 12:59:27 -0700

    feat(app): add NetworkSelector component (#115)

---
+++ packages/app/src/systems/Sidebar/components/NetworkSelector/NetworkSelector.test.tsx
@@ -0,0 +1,57 @@
+import { render, screen, testA11y } from "@fuel-ui/test-utils";
+import { BrowserRouter } from "react-router-dom";
+
+import { NetworkSelector } from "./NetworkSelector";
+
+const NETWORKS = [
+  {
+    id: 1,
+    isSelected: true,
+    isOnline: true,
+    name: "Mainnet",
+    url: "https://node.fuel.network/graphql",
+  },
+  {
+    id: 2,
+    name: "Localhost",
+    url: "http://localhost:4000",
+  },
+];
+
+const props = {
+  networks: NETWORKS,
+  selected: NETWORKS[0],
+};
+
+describe("NetworkSelector", () => {
+  it("a11y", async () => {
+    await testA11y(<NetworkSelector {...props} />, {
+      wrapper: BrowserRouter,
+    });
+  });
+
+  it("should open dropdown with given networks", async () => {
+    const { user } = render(<NetworkSelector {...props} />, {
+      wrapper: BrowserRouter,
+    });
+    expect(() => screen.getByText("Localhost")).toThrow();
+    const selector = screen.getByTestId("fuel_network-item");
+    await user.click(selector);
+    expect(screen.getByText("Localhost")).toBeInTheDocument();
+  });
+
+  it("should dispatch onSelectNetwork handle", async () => {
+    const handler = jest.fn();
+    const { user } = render(
+      <NetworkSelector {...props} onSelectNetwork={handler} />,
+      { wrapper: BrowserRouter }
+    );
+
+    const selector = screen.getByTestId("fuel_network-item");
+    await user.click(selector);
+    const item = screen.getByText("Localhost");
+    expect(item).toBeInTheDocument();
+    await user.click(item);
+    expect(handler).toBeCalledWith(NETWORKS[1]);
+  });
+});
---
+++ packages/app/src/systems/Sidebar/components/NetworkSelector/NetworkSelector.tsx
@@ -0,0 +1,79 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+import { cssObj } from "@fuel-ui/css";
+import { Dropdown, Flex, Icon, Text } from "@fuel-ui/react";
+import { useEffect, useState } from "react";
+
+import type { Network } from "~/systems/Network";
+import { NetworkStatus, NetworkItem } from "~/systems/Network";
+
+export type NetworkSelectorProps = {
+  selected: Network;
+  networks: Network[];
+  onSelectNetwork?: (network: Network) => void;
+};
+
+export function NetworkSelector({
+  selected,
+  networks,
+  onSelectNetwork,
+}: NetworkSelectorProps) {
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    const selector = document.querySelector(
+      "[data-testid='fuel_network-item']"
+    );
+    selector && setWidth(selector?.clientWidth);
+  }, []);
+
+  return (
+    <Flex css={styles.root}>
+      <Text as="div" leftIcon={Icon.is("ShareNetwork")}>
+        Network selected
+      </Text>
+      <Dropdown>
+        <Dropdown.Trigger>
+          <NetworkItem network={selected!} css={styles.button} />
+        </Dropdown.Trigger>
+        <Dropdown.Menu
+          autoFocus
+          disabledKeys={["edit"]}
+          aria-label="Actions"
+          css={{ width }}
+          onAction={(id) => {
+            const network = networks.find((n) => n.id.toString() === id);
+            network && onSelectNetwork?.(network);
+          }}
+        >
+          {networks.map((network) => (
+            <Dropdown.MenuItem key={network.id} textValue={network.name}>
+              <NetworkStatus network={network} />
+              {network.name}
+            </Dropdown.MenuItem>
+          ))}
+        </Dropdown.Menu>
+      </Dropdown>
+    </Flex>
+  );
+}
+
+const styles = {
+  root: cssObj({
+    flexDirection: "column",
+    gap: "$3",
+
+    "& > .fuel_text": {
+      fontSize: "$sm",
+    },
+  }),
+  button: cssObj({
+    fontSize: "$lg",
+
+    "&:hover": {
+      cursor: "pointer",
+    },
+    "&::after": {
+      background: "transparent",
+    },
+  }),
+};
