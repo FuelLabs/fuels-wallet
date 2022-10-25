51d8effa2ce025469d8a48b823662e4d8bf670b2
Luiz Estacio <luizstacio@gmail.com>
2022-10-25 15:24:43 -0300

    chore: revert changes

--- packages/app/playwright.config.ts
+++ packages/app/playwright.config.ts
@@ -6,8 +6,8 @@ const { E2E_PORT = 9000 } = process.env;
 const distDirectory = join(__dirname, './dist');
 
 const config: PlaywrightTestConfig = {
-  testMatch: join(__dirname, './playwright/e2e/**/*.test.ts'),
-  testDir: join(__dirname, './playwright/e2e/'),
+  testMatch: join(__dirname, './playwright/**/*.test.ts'),
+  testDir: join(__dirname, './playwright/'),
   webServer: {
     command: `pnpm exec http-server -s -p ${E2E_PORT} ${distDirectory}`,
     port: Number(E2E_PORT),
--- packages/app/playwright/commons/visit.ts
+++ packages/app/playwright/commons/visit.ts
@@ -1,10 +1,9 @@
 import type { Page } from '@playwright/test';
 
 export async function visit(page: Page, pathname: string) {
-  const pageFinal = await page.goto(pathname);
-  // Wait for 100ms as the current wait for event networkidle
-  // don't correct work on CI
-  await page.waitForTimeout(1000);
+  const pageFinal = await page.goto(pathname, {
+    waitUntil: 'networkidle',
+  });
   return pageFinal;
 }
 
