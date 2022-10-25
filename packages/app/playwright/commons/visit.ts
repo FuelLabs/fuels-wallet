38f6a04fb11029a26ca5e9d36baa90227d29a144
Luiz Estacio <luizstacio@gmail.com>
2022-10-25 19:25:17 -0300

    chore: fix act and page visit

--- packages/app/playwright/commons/visit.ts
+++ packages/app/playwright/commons/visit.ts
@@ -1,9 +1,9 @@
 import type { Page } from '@playwright/test';
 
 export async function visit(page: Page, pathname: string) {
-  const pageFinal = await page.goto(pathname, {
-    waitUntil: 'networkidle',
-  });
+  const pageFinal = await page.goto(pathname);
+  // Using waitUntil: 'networkidle' has interminent issues
+  await page.waitForTimeout(500);
   return pageFinal;
 }
 
