a371e386238b4efe8fba369aa381e745508103b8
Matt Auer <mattauer@Matts-MacBook-Pro.local>
2022-11-08 14:05:59 -0600

    fix: loader

--- packages/app/src/systems/Asset/components/AssetInput/AmountInputLoader.tsx
+++ packages/app/src/systems/Asset/components/AssetInput/AmountInputLoader.tsx
@@ -4,19 +4,19 @@ import type { IContentLoaderProps } from 'react-content-loader';
 import ContentLoader from 'react-content-loader';
 
 export const AmountInputLoader = (props: IContentLoaderProps) => (
-  <CardList.Item css={{ padding: 0 }}>
+  <CardList.Item css={{ padding: 0, background: darkColors.gray2 }}>
     <ContentLoader
       speed={2}
       width={300}
-      height={60}
-      viewBox="0 0 300 60"
+      height={65}
+      viewBox="0 0 300 65"
       backgroundColor={darkColors.gray3}
       foregroundColor={darkColors.gray4}
       {...props}
     >
       <rect x="246" y="12" rx="4" ry="4" width="36" height="14" />
       <rect x="194" y="36" rx="4" ry="4" width="92" height="14" />
-      <rect x="10" y="18" rx="4" ry="4" width="92" height="24" />
+      <rect x="10" y="21" rx="4" ry="4" width="92" height="24" />
     </ContentLoader>
   </CardList.Item>
 );
