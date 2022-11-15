8462a19e214d902f651b7eefe18c14409a8e08a8
Pedro Nauck <pedronauck@gmail.com>
2022-11-14 21:32:13 -0800

    chore(docs): use prism-react-rendered instead of prismjs

--- packages/docs/src/components/Pre.tsx
+++ packages/docs/src/components/Pre.tsx
@@ -1,28 +1,76 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { cssObj } from '@fuel-ui/css';
+import { cssObj, styled } from '@fuel-ui/css';
 import { Box } from '@fuel-ui/react';
-import Prism from 'prismjs';
-import { useEffect } from 'react';
+import Highlight, { defaultProps } from 'prism-react-renderer';
+import { Children } from 'react';
 
-import 'prismjs/components/prism-javascript';
-import 'prismjs/components/prism-typescript';
-import 'prismjs/components/prism-css';
-import 'prismjs/components/prism-jsx';
+import { prismTheme } from '../styles/prismTheme';
+
+const Line = styled(Box, {
+  display: 'table-row',
+});
+
+const LineNo = styled(Box, {
+  display: 'table-cell',
+  textAlign: 'right',
+  pr: '$2',
+  userSelect: 'none',
+  opacity: '0.5',
+});
+
+const LineContent = styled(Box, {
+  display: 'table-cell',
+});
 
 export function Pre(props: any) {
-  useEffect(() => {
-    Prism.highlightAll();
-  }, []);
+  const codeEl: any = Children.toArray(props.children).find((child: any) => {
+    return child.type.name === 'Code';
+  });
 
-  return <Box as="pre" css={styles.root} {...props} />;
+  const code = codeEl?.props.children;
+  const language = codeEl?.props.className.replace('language-', '');
+
+  return (
+    <Highlight
+      {...defaultProps}
+      code={code}
+      language={language}
+      theme={prismTheme}
+    >
+      {({ className, style, tokens, getLineProps, getTokenProps }) => {
+        return (
+          <Box as="pre" className={className} style={style} css={styles.pre}>
+            {tokens.map((line, i) => {
+              if (i === tokens.length - 1) return null;
+              return (
+                <Line key={i} {...getLineProps({ line, key: i })}>
+                  <LineNo as="span">{i + 1}</LineNo>
+                  <LineContent as="span">
+                    {line.map((token, key) => {
+                      return (
+                        <Box
+                          as="span"
+                          key={key}
+                          {...getTokenProps({ token, key })}
+                        />
+                      );
+                    })}
+                  </LineContent>
+                </Line>
+              );
+            })}
+          </Box>
+        );
+      }}
+    </Highlight>
+  );
 }
 
 const styles = {
-  root: cssObj({
-    mb: '$6 !important',
-
-    code: {
-      padding: '$0',
+  pre: cssObj({
+    mb: '$5 !important',
+    '.token.plain': {
+      color: '$gray11',
     },
   }),
 };
