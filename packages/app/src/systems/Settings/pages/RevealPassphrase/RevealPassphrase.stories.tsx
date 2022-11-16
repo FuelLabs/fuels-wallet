0ee5e783902f351f3d408f4d844c9311084246ab
LFSCamargo <luizepauloxd@gmail.com>
2022-11-15 23:14:01 -0300

    fix: typo on storybook file

--- packages/app/src/systems/Settings/pages/RevealPassphrase/RevealPassphrase.stories.tsx
+++ packages/app/src/systems/Settings/pages/RevealPassphrase/RevealPassphrase.stories.tsx
@@ -1,13 +1,13 @@
 import type { StoryFn } from '@storybook/react';
 
-import { RecoverPassphrase } from './RevealPassphrase';
+import { RevealPassphrase } from './RevealPassphrase';
 
 export default {
-  component: RecoverPassphrase,
-  title: 'SignUp/Pages/1. RecoverPassphrase',
+  component: RevealPassphrase,
+  title: 'SignUp/Pages/1. RevealPassphrase',
   parameters: {
     layout: 'fullscreen',
   },
 };
 
-export const Usage: StoryFn<unknown> = () => <RecoverPassphrase />;
+export const Usage: StoryFn<unknown> = () => <RevealPassphrase />;
