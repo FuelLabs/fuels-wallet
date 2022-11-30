dc4c303ac8f025ff1380ece23f0037d36d6f5d07
luizstacio <luizstacio@gmail.com>
2022-11-29 19:33:11 -0800

    feat: update index name config

--- packages/docs/src/components/Search.tsx
+++ packages/docs/src/components/Search.tsx
@@ -1,9 +1,11 @@
 import { DocSearch } from '@docsearch/react';
 
+import { index_name } from '~/docsearch.json';
+
 export function Search() {
   return (
     <DocSearch
-      indexName="wallet_docs"
+      indexName={index_name}
       appId={process.env.NEXT_PUBLIC_ALGOLIA_APP_ID!}
       apiKey={process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY!}
       disableUserPersonalization={true}
