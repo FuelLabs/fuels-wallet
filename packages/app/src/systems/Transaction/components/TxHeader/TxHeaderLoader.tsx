f7287cd21ce9f29f60939d22ff2c93aae480e410
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2022-11-30 01:07:12 -0300

    fix: spacing

--- packages/app/src/systems/Transaction/components/TxHeader/TxHeaderLoader.tsx
+++ packages/app/src/systems/Transaction/components/TxHeader/TxHeaderLoader.tsx
@@ -8,15 +8,15 @@ export const TxHeaderLoader = (props: IContentLoaderProps) => (
     <ContentLoader
       speed={2}
       width={300}
-      height={98}
-      viewBox="0 0 300 100"
+      height={74}
+      viewBox="0 0 300 76"
       backgroundColor={darkColors.gray2}
       foregroundColor={darkColors.gray3}
       {...props}
     >
-      <rect x="12" y="25" width="150" height="20" rx="4" />
-      <rect x="12" y="63" width="100" height="20" rx="4" />
-      <rect x="225" y="25" width="50" height="20" rx="4" />
+      <rect x="12" y="13" width="150" height="20" rx="4" />
+      <rect x="12" y="45" width="100" height="20" rx="4" />
+      <rect x="225" y="13" width="50" height="20" rx="4" />
     </ContentLoader>
   </Card>
 );
