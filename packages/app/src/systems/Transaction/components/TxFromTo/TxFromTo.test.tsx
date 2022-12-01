87e9bab4304afbd1605673b3199a9ad67b968eb6
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2022-12-01 01:35:34 -0300

    feat: address TxFromTo requirements to Transaction Screen

--- packages/app/src/systems/Transaction/components/TxFromTo/TxFromTo.test.tsx
+++ packages/app/src/systems/Transaction/components/TxFromTo/TxFromTo.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen, testA11y } from '@fuel-ui/test-utils';
 
 import { MOCK_TX_RECIPIENT } from '../../__mocks__/tx-recipient';
-import { TxState } from '../../types';
+import { TxStatus } from '../../types';
 
 import { TxFromTo } from './TxFromTo';
 
@@ -15,19 +15,48 @@ describe('TxFromTo', () => {
     await testA11y(<TxFromTo {...PROPS} />);
   });
 
-  it('should render both cards correctly', async () => {
+  it('should render both cards correctly and dont have spinner', async () => {
     render(<TxFromTo {...PROPS} />);
     expect(screen.getByText('From')).toBeInTheDocument();
     expect(screen.getByText('fuel1y...y6wk')).toBeInTheDocument();
     expect(screen.getByText('To (Contract)')).toBeInTheDocument();
     expect(screen.getByText('0x277f...207c')).toBeInTheDocument();
+    expect(() => screen.getByLabelText('Loading Spinner')).toThrow();
   });
 
-  it('should show a spinner when state is pending', async () => {
-    render(<TxFromTo {...PROPS} state={TxState.pending} />);
+  it('should not show any address info and show spinner when isLoading is true', async () => {
+    render(<TxFromTo {...PROPS} isLoading />);
     expect(() => screen.getByText('From')).toThrow();
     expect(() => screen.getByText('fuel1y...y6wk')).toThrow();
     expect(() => screen.getByText('To (Contract)')).toThrow();
     expect(() => screen.getByText('0x277f...207c')).toThrow();
+    expect(() => screen.getByLabelText('Loading Spinner')).toBeInTheDocument();
+  });
+
+  it('should show address info and show spinner when status is pending', async () => {
+    render(<TxFromTo {...PROPS} status={TxStatus.pending} />);
+    expect(screen.getByText('From')).toBeInTheDocument();
+    expect(screen.getByText('fuel1y...y6wk')).toBeInTheDocument();
+    expect(screen.getByText('To (Contract)')).toBeInTheDocument();
+    expect(screen.getByText('0x277f...207c')).toBeInTheDocument();
+    expect(() => screen.getByLabelText('Loading Spinner')).toBeInTheDocument();
+  });
+
+  it('should show address info and not have spinner when status is success', async () => {
+    render(<TxFromTo {...PROPS} status={TxStatus.pending} />);
+    expect(screen.getByText('From')).toBeInTheDocument();
+    expect(screen.getByText('fuel1y...y6wk')).toBeInTheDocument();
+    expect(screen.getByText('To (Contract)')).toBeInTheDocument();
+    expect(screen.getByText('0x277f...207c')).toBeInTheDocument();
+    expect(() => screen.getByLabelText('Loading Spinner')).toThrow();
+  });
+
+  it('should show address info and not have spinner when status is error', async () => {
+    render(<TxFromTo {...PROPS} status={TxStatus.pending} />);
+    expect(screen.getByText('From')).toBeInTheDocument();
+    expect(screen.getByText('fuel1y...y6wk')).toBeInTheDocument();
+    expect(screen.getByText('To (Contract)')).toBeInTheDocument();
+    expect(screen.getByText('0x277f...207c')).toBeInTheDocument();
+    expect(() => screen.getByLabelText('Loading Spinner')).toThrow();
   });
 });
--- packages/app/src/systems/Transaction/components/TxRecipientCard/TxRecipientCardLoader.tsx
+++ packages/app/src/systems/Transaction/components/TxRecipientCard/TxRecipientCardLoader.tsx
@@ -1,35 +1,23 @@
-import { cssObj, darkColors } from '@fuel-ui/css';
+import { darkColors } from '@fuel-ui/css';
 import { Card } from '@fuel-ui/react';
 import type { IContentLoaderProps } from 'react-content-loader';
 import ContentLoader from 'react-content-loader';
 
 export const TxRecipientCardLoader = (props: IContentLoaderProps) => (
-  <Card css={styles.root}>
+  <Card>
     <ContentLoader
+      style={{ border: 0 }}
       speed={2}
-      width={92}
-      height={105}
-      viewBox="0 0 92 104"
+      width={149}
+      height={154}
+      viewBox="0 0 149 156"
       backgroundColor={darkColors.gray2}
       foregroundColor={darkColors.gray3}
       {...props}
     >
-      <rect x="15" width="62" height="14" rx="4" />
-      <rect x="20" y="26" width="52" height="52" rx="26" />
-      <rect x="20" y="26" width="52" height="52" rx="26" />
-      <rect y="92" width="92" height="14" rx="4" />
+      <rect x="48" y="16" width="56" height="20" rx="4" />
+      <rect x="48" y="48" width="56" height="56" rx="28" />
+      <rect x="35" y="120" width="80" height="20" rx="4" />
     </ContentLoader>
   </Card>
 );
-
-const styles = {
-  root: cssObj({
-    minWidth: '130px',
-    minHeight: '122px',
-    py: '$4',
-    px: '$3',
-    display: 'inline-flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }),
-};
