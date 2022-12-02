ab70492f7b5116335a98b4602e35d06ddc171352
Luiz Felipe Bolsoni Gomes <8636507+LuizAsFight@users.noreply.github.com>
2022-12-02 00:57:29 -0300

    feat: add AssetAmount loader (#294)

---
+++ packages/app/src/systems/Asset/components/AssetsAmount/AssetsAmountLoader.tsx
@@ -0,0 +1,22 @@
+import { darkColors } from '@fuel-ui/css';
+import { Card } from '@fuel-ui/react';
+import type { IContentLoaderProps } from 'react-content-loader';
+import ContentLoader from 'react-content-loader';
+
+export const AssetsAmountLoader = (props: IContentLoaderProps) => (
+  <Card>
+    <ContentLoader
+      speed={2}
+      width={300}
+      height={84}
+      viewBox="0 0 300 86"
+      backgroundColor={darkColors.gray2}
+      foregroundColor={darkColors.gray3}
+      {...props}
+    >
+      <rect x="12" y="18" width="100" height="20" rx="4" />
+      <rect x="12" y="50" width="125" height="20" rx="4" />
+      <rect x="189" y="34" width="100" height="20" rx="4" />
+    </ContentLoader>
+  </Card>
+);
