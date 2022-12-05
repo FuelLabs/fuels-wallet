d981aae794e8a4ae18bac2fe66a3ba44293f10af
Luiz Felipe Bolsoni Gomes <8636507+LuizAsFight@users.noreply.github.com>
2022-12-05 00:52:48 -0300

    feat: add TxStatusAlert (#309)

---
+++ packages/app/src/systems/Transaction/components/TxStatusAlert/TxStatusAlert.stories.tsx
@@ -0,0 +1,21 @@
+import { TxStatus } from '../../types';
+
+import type { TxStatusAlertProps } from './TxStatusAlert';
+import { TxStatusAlert } from './TxStatusAlert';
+
+export default {
+  component: TxStatusAlert,
+  title: 'Transaction/Components/TxStatusAlert',
+};
+
+export const Failed = (args: TxStatusAlertProps) => (
+  <TxStatusAlert {...args} txStatus={TxStatus.error} txId={'0xoaskokaes'} />
+);
+
+export const Pending = (args: TxStatusAlertProps) => (
+  <TxStatusAlert {...args} txStatus={TxStatus.pending} txId={'0xoaskokaes'} />
+);
+
+export const Error = (args: TxStatusAlertProps) => (
+  <TxStatusAlert {...args} error={'Invalid Transaction ID'} />
+);
