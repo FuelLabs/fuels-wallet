cdaa30d1dcdee29af679c50a4190494f8602ffda
luizstacio <luizstacio@gmail.com>
2022-12-06 13:03:54 -0800

    test: add tests for invalid and not match seed phrase

---
+++ packages/app/src/systems/Core/__tests__/utils/expectStateMatch.ts
@@ -0,0 +1,13 @@
+import type { AnyInterpreter, StateFrom } from 'xstate';
+import { waitFor } from 'xstate/lib/waitFor';
+
+export async function expectStateMatch<
+  I extends AnyInterpreter,
+  T extends StateFrom<I['machine']>,
+  D extends I['machine']['__TResolvedTypesMeta']['resolved']['matchesStates']
+>(service: I, stateName: D) {
+  const state: T = await waitFor(service, (state: T) =>
+    state.matches(stateName)
+  );
+  expect(state.matches(stateName)).toBeTruthy();
+}
