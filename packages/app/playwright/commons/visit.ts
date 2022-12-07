e54c50e54e6845078461fea2517fb12f8e91e92b
tomide <ayotomiiide@gmail.com>
2022-12-07 11:43:51 +0000

    test: update page reload wait time to 10s

--- packages/app/playwright/commons/visit.ts
+++ packages/app/playwright/commons/visit.ts
@@ -3,7 +3,7 @@ import type { Page } from '@playwright/test';
 export async function visit(page: Page, pathname: string) {
   const pageFinal = await page.goto(pathname);
   // Using waitUntil: 'networkidle' has interminent issues
-  await page.waitForTimeout(500);
+  await page.waitForTimeout(1000);
   return pageFinal;
 }
 
