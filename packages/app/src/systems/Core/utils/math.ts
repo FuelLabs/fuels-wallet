ee575c327fa5b57a4be87d9e1541416d6f43b680
luizstacio <luizstacio@gmail.com>
2022-12-06 20:57:27 -0800

    feat: update fuels version to use format

--- packages/app/src/systems/Core/utils/math.ts
+++ packages/app/src/systems/Core/utils/math.ts
@@ -1,17 +1,14 @@
 import type { BNInput } from 'fuels';
 import { bn } from 'fuels';
 
-import { DECIMAL_UNITS, MAX_FRACTION_DIGITS } from '~/config';
+import { MAX_FRACTION_DIGITS } from '~/config';
 
 export function formatAmount(amount?: BNInput) {
   return bn(amount).format({
-    minPrecision: MAX_FRACTION_DIGITS,
+    precision: MAX_FRACTION_DIGITS,
   });
 }
 
 export function formatFullAmount(amount?: BNInput) {
-  return bn(amount).format({
-    minPrecision: MAX_FRACTION_DIGITS,
-    precision: DECIMAL_UNITS,
-  });
+  return bn(amount).format();
 }
