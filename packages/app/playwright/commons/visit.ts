f6ac756c3d11649b042a5347c0e014983b1d8af0
luizstacio <luizstacio@gmail.com>
2022-12-06 23:03:20 -0800

    fix: reduce the time to reload

--- packages/app/playwright/commons/visit.ts
+++ packages/app/playwright/commons/visit.ts
@@ -3,7 +3,7 @@ import type { Page } from '@playwright/test';
 export async function visit(page: Page, pathname: string) {
   const pageFinal = await page.goto(pathname);
   // Using waitUntil: 'networkidle' has interminent issues
-  await page.waitForTimeout(1000);
+  await page.waitForTimeout(500);
   return pageFinal;
 }
 
@@ -15,6 +15,6 @@ export async function waitUrl(page: Page, pathname: string) {
 
 export async function reload(page: Page) {
   await page.reload();
-  await page.waitForTimeout(1000);
+  await page.waitForTimeout(500);
   return page;
 }
