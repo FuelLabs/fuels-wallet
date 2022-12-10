e3368bf0d353cfa89c3216978cfe1d948c23ff98
luizstacio <luizstacio@gmail.com>
2022-12-09 17:04:12 -0800

    fix: check if wallet is setup before open connection

--- packages/app/src/systems/Asset/utils/constants.tsx
+++ packages/app/src/systems/Asset/utils/constants.tsx
@@ -1,31 +1,40 @@
-import type { Asset } from '@fuel-wallet/types';
-
-import { relativeUrl } from '~/systems/Core';
-
 /**
  * TODO: change this for something automatic, this is here just for development purposes
  */
-export const ASSET_MAP: Record<string, Asset> = {
-  '0x0000000000000000000000000000000000000000000000000000000000000000': {
+import type { Asset } from '@fuel-wallet/types';
+
+import { relativeUrl } from '~/systems/Core/utils';
+
+// When exporting Object.values from a file, the bundler for
+// the chrome extension tries to bundle it as a window object
+// failing the bundle. This way the bundler works fine.
+export const ASSET_LIST: Array<Asset> = [
+  {
     assetId:
       '0x0000000000000000000000000000000000000000000000000000000000000000',
     name: 'Ethereum',
     symbol: 'ETH',
     imageUrl: relativeUrl('assets/eth.svg'),
   },
-  '0xe90f62d5216f58ffaf19e6eba07f944cc2cd36f9800b89419d52b6927f53976701': {
+  {
     assetId:
       '0xe90f62d5216f58ffaf19e6eba07f944cc2cd36f9800b89419d52b6927f53976701',
     name: 'Dai',
     symbol: 'DAI',
     imageUrl: relativeUrl('assets/dai.svg'),
   },
-  '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599': {
+  {
     assetId: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
     name: 'Bitcoin',
     symbol: 'BTC',
     imageUrl: relativeUrl('assets/btc.svg'),
   },
-};
+];
 
-export const ASSET_LIST = Object.values(ASSET_MAP);
+export const ASSET_MAP: Record<string, Asset> = {
+  '0x0000000000000000000000000000000000000000000000000000000000000000':
+    ASSET_LIST[0],
+  '0xe90f62d5216f58ffaf19e6eba07f944cc2cd36f9800b89419d52b6927f53976701':
+    ASSET_LIST[1],
+  '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599': ASSET_LIST[2],
+};
--- packages/app/src/systems/CRX/background/services/BackgroundService.ts
+++ packages/app/src/systems/CRX/background/services/BackgroundService.ts
@@ -1,12 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { CONTENT_SCRIPT_NAME, MessageTypes } from '@fuel-wallet/types';
 import type { FuelWeb3ProviderConfig } from '@fuel-wallet/types';
-import type { JSONRPCParams } from 'json-rpc-2.0';
+import type {
+  JSONRPCParams,
+  JSONRPCRequest,
+  JSONRPCServerMiddlewareNext,
+} from 'json-rpc-2.0';
 import { JSONRPCServer } from 'json-rpc-2.0';
 
 import type { CommunicationProtocol } from './CommunicationProtocol';
 import { PopUpService } from './PopUpService';
 
+import { AccountService } from '~/systems/Account/services';
 import { Pages } from '~/systems/Core/types';
 import { ConnectionService } from '~/systems/DApp/services';
 
@@ -19,6 +24,7 @@ export class BackgroundService {
   constructor(communicationProtocol: CommunicationProtocol) {
     this.communicationProtocol = communicationProtocol;
     this.server = new JSONRPCServer<EventOrigin>();
+    this.server.applyMiddleware(this.connectionMiddlware.bind(this));
     this.setupListeners();
     this.externalMethods([
       this.accounts,
@@ -36,10 +42,6 @@ export class BackgroundService {
   setupListeners() {
     this.communicationProtocol.on(MessageTypes.request, async (event) => {
       const origin = event.sender!.origin!;
-      // If the method is not connect check if connection is already established
-      if (event.request.method !== 'connect') {
-        await this.requireConnection(origin);
-      }
       const response = await this.server.receive(event.request, {
         origin,
       });
@@ -64,11 +66,34 @@ export class BackgroundService {
     });
   }
 
+  async requireAccounts() {
+    const accounts = await AccountService.getAccounts();
+    if (accounts.length === 0) {
+      throw new Error('Unable to establish a connection. No accounts found');
+    }
+  }
+
   async requireConnection(origin: string) {
     const authorizedApp = await ConnectionService.getConnection(origin);
     if (!authorizedApp) {
-      throw new Error('Applicaiton not connected');
+      throw new Error(
+        'Connection not established. Please call connect() first to request a connection'
+      );
     }
+  }
+
+  async connectionMiddlware(
+    next: JSONRPCServerMiddlewareNext<EventOrigin>,
+    request: JSONRPCRequest,
+    serverParams: EventOrigin | undefined
+  ) {
+    // If the method is not connect check if connection is already established
+    if (request.method !== 'connect') {
+      await this.requireConnection(serverParams!.origin);
+    } else {
+      await this.requireAccounts();
+    }
+    return next(request, serverParams);
   }
 
   async sendEvent(origin: string, eventName: string, params: any[]) {
