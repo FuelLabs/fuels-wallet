1885f39ce9ed84a1ab3fa23fe075a76938bcedad
Tomide Oladipo <ayotomiiide@gmail.com>
2022-12-13 20:53:57 +0000

    docs: add browser support section to the docs (#358)

--- packages/docs/src/components/Table.tsx
+++ packages/docs/src/components/Table.tsx
@@ -7,6 +7,26 @@ export function Table(props: any) {
   return <Box as="table" {...props} css={styles.root} />;
 }
 
+export function TD(props: any) {
+  return (
+    <Box as="td" {...props} css={styles.td}>
+      <Box as="div" css={styles.tdContainer}>
+        {props.children}
+      </Box>
+    </Box>
+  );
+}
+
+export function TH(props: any) {
+  return (
+    <Box as="th" {...props} css={styles.td}>
+      <Box as="div" css={styles.thContainer}>
+        {props.children}
+      </Box>
+    </Box>
+  );
+}
+
 const styles = {
   root: cssObj({
     py: '$3',
@@ -15,5 +35,24 @@ const styles = {
     background: '$gray1',
     borderRadius: '$lg',
     border: '1px solid $gray3',
+    width: '100%',
+  }),
+  td: cssObj({
+    py: '$2',
+    px: '$3',
+  }),
+  tdContainer: cssObj({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+  }),
+  thContainer: cssObj({
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    gap: '$2',
+    justifyContent: 'space-between',
+    height: '100%',
   }),
 };
