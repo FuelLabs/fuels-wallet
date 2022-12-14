2b4754996354b7d1791ae23fee309bfcf79b9a49
Matt Auer <mattauer@umich.edu>
2022-12-13 19:35:11 -0600

    feat: add account form with values story

--- packages/app/src/systems/Account/components/AccountForm/AccountForm.stories.tsx
+++ packages/app/src/systems/Account/components/AccountForm/AccountForm.stories.tsx
@@ -1,5 +1,6 @@
 import { Box } from '@fuel-ui/react';
 
+import { MOCK_ACCOUNTS } from '../../__mocks__';
 import { useAccountForm } from '../../hooks/useAccountForm';
 
 import { AccountForm } from './AccountForm';
@@ -11,6 +12,17 @@ export default {
 
 export const Usage = () => {
   const form = useAccountForm();
+  return (
+    <Box css={{ width: 320 }}>
+      <AccountForm form={form} />
+    </Box>
+  );
+};
+
+export const WithValues = () => {
+  const form = useAccountForm({
+    defaultValues: MOCK_ACCOUNTS[0],
+  });
   return (
     <Box css={{ width: 320 }}>
       <AccountForm form={form} />
