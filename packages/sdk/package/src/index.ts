98efba67d90b98b92154c3cab44e69d2b47ddada
luizstacio <luizstacio@gmail.com>
2023-01-18 04:56:29 -0300

    chore: delete files

--- packages/sdk/package/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from '@fuel-wallet/types';
-export * from './Fuel';
-export * from './FuelWalletConnection';
-export * from './FuelWalletProvider';
-export * from './utils';
-export * from './connections';
