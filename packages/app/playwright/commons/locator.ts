826736b2572abc14ec5b062c505b6c449746903b
luizstacio <luizstacio@gmail.com>
2023-01-28 01:04:19 -0300

    fix: remove to be visible from e2e tests

--- packages/app/playwright/commons/locator.ts
+++ packages/app/playwright/commons/locator.ts
@@ -1,14 +1,11 @@
 import type { Page } from '@playwright/test';
-import { expect } from '@playwright/test';
 
 export function getByAriaLabel(page: Page, selector: string) {
   return page.locator(`[aria-label="${selector}"]`);
 }
 
 export async function waitAriaLabel(page: Page, selector: string) {
-  const locator = page.locator(`[aria-label="${selector}"]`);
-  await expect(locator).toBeVisible();
-  return locator;
+  return page.waitForSelector(`[aria-label="${selector}"]`);
 }
 
 export function getInputByName(page: Page, name: string) {
