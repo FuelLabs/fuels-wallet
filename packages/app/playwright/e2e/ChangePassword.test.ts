08460a3d4af8506f4cccbdde5c2da13709dc89ad
luizstacio <luizstacio@gmail.com>
2023-01-30 01:33:12 -0300

    test: change to check by success on change pass

--- packages/app/playwright/e2e/ChangePassword.test.ts
+++ packages/app/playwright/e2e/ChangePassword.test.ts
@@ -30,7 +30,7 @@ test.describe('ChangePassword', () => {
     // submit data
     await hasText(page, 'Save');
     await getButtonByText(page, 'Save').click();
-    await hasText(page, /assets/i, 1);
+    await hasText(page, 'Password Changed');
   });
 
   test('should not change the user password when passwords not the same', async () => {
