1a429da9f614479db4f90a16e60711365be06eb2
Pedro Nauck <pedronauck@gmail.com>
2023-01-31 21:46:08 -0300

    chore: improve project tooling (#513)
    ## Tasks
    
    This pull request makes some changes to improve precise details on our
    tooling
    
    ### `.lintstagedrc`
    - [x] Change to `prettier --write` and `eslint --fix`. So, instead of
    just checking on husky, it will format and fix the files before
    committing.
    - [x] Add other formats on `lintstaged` to run `prettier`
    
    ### `.prettierrc`
    - [x] Setup prettier to format all types of files, since we have a bunch
    new of them and this configuration came from `fuels-ts` that's a totally
    different type of project
    
    ### `.vscode` folder
    - [x] Add a recommendation of a new extension that is a [Jest
    runner](https://marketplace.visualstudio.com/items?itemName=firsttris.vscode-jest-runner)
    inside VSCode. I used this extension on `fuel-ui` and it's capable to
    debug using VSCode and Jest, it's very useful for debugging without
    console.log on tests, and this extension is way better than the with
    most downloads on VSCode extensions store.

--- packages/fuelhat/index.mjs
+++ packages/fuelhat/index.mjs
@@ -1,26 +1,26 @@
 #!/usr/bin/env node
 
-import yargs from "yargs";
-import { hideBin } from "yargs/helpers";
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 
-import { commands } from "./commands/index.mjs";
+import { commands } from './commands/index.mjs';
 
 yargs(hideBin(process.argv))
-  .scriptName("fuelhat")
-  .usage("$0 <cmd> [args]")
-  .command("node <cmd>", "Start or stop a Fuel local node", (yargs) => {
-    yargs.option("project", {
-      alias: "p",
-      default: "fuelhat",
-      describe: "Project name",
+  .scriptName('fuelhat')
+  .usage('$0 <cmd> [args]')
+  .command('node <cmd>', 'Start or stop a Fuel local node', (yargs) => {
+    yargs.option('project', {
+      alias: 'p',
+      default: 'fuelhat',
+      describe: 'Project name',
     });
-    yargs.option("test", {
-      alias: "t",
-      describe: "Run using test environment",
+    yargs.option('test', {
+      alias: 't',
+      describe: 'Run using test environment',
     });
-    yargs.option("debug", {
-      alias: "d",
-      describe: "Show docker logs",
+    yargs.option('debug', {
+      alias: 'd',
+      describe: 'Show docker logs',
     });
     yargs.command(commands).demandCommand(1);
   })
