798d70fa4fb6c2f964e11b13592beef29efd20e0
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2023-02-03 19:50:35 -0300

    feat: start event to inform it's loaded

--- packages/app/src/systems/CRX/scripts/contentScript.ts
+++ packages/app/src/systems/CRX/scripts/contentScript.ts
@@ -1,14 +1,28 @@
-import { ContentProxyConnection } from '@fuel-wallet/sdk';
+import { ContentProxyConnection, MessageTypes } from '@fuel-wallet/sdk';
 
 import fileName from './pageScript?script&module';
 
-ContentProxyConnection.start();
+const contentProxyConnection = ContentProxyConnection.start();
 
 async function main() {
   const script = document.createElement('script');
   script.src = chrome.runtime.getURL(fileName);
   script.type = 'module';
   script.onload = () => {
+    console.log('fuel script loaded first time');
+    setTimeout(() => {
+      console.log('send event to doc');
+      contentProxyConnection.postMessage({
+        type: MessageTypes.event,
+        events: [
+          {
+            event: 'fuelLoaded',
+            params: [],
+          },
+        ],
+        target: 'a',
+      });
+    }, 3000);
     script.remove();
   };
   (document.head || document.documentElement).appendChild(script);
--- packages/docs/src/hooks/useFuel.tsx
+++ packages/docs/src/hooks/useFuel.tsx
@@ -2,12 +2,18 @@ import { useState, useEffect } from 'react';
 
 const globalWindow = typeof window !== 'undefined' ? window : ({} as Window);
 
+console.log('useFuel file started');
 export function useFuel() {
+  console.log('first render of useFuel');
   const [error, setError] = useState('');
   const [isLoading, setLoading] = useState(true);
   const [fuel, setFuel] = useState<Window['fuel']>(globalWindow.fuel);
 
   useEffect(() => {
+    window.addEventListener('event', (data) => {
+      console.log(`data`, data);
+    });
+
     const timeout = setTimeout(() => {
       if (globalWindow.fuel) {
         setFuel(globalWindow.fuel);
