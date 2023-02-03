7795780c79777b24ce54a80239b2f8a6e7a06253
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2023-02-03 02:07:07 -0300

    feat: change doc

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -3,13 +3,11 @@
 # User should define here the Dependencies to link locally
 # we support linking "@fuel-ui" and "@fuels-ts". 
 LINK_FUEL_UI=true
-LINK_FUEL_TS=true
-
+# LINK_FUEL_TS=true
 
 # Figure out dependencies to link
 deps=""
 if [ "$LINK_FUEL_TS" = true ] ; then
-    # fuel_ts_deps="fuels @fuel-ts/mnemonic @fuel-ts/providers @fuel-ts/wallet @fuel-ts/wallet-manager"
     fuel_ts_deps="fuels @fuel-ts/abi-coder @fuel-ts/abi-typegen @fuel-ts/address @fuel-ts/constants @fuel-ts/contract @fuel-ts/hasher @fuel-ts/hdwallet @fuel-ts/interfaces @fuel-ts/keystore @fuel-ts/math @fuel-ts/merkle-shared @fuel-ts/merklesum @fuel-ts/mnemonic @fuel-ts/predicate @fuel-ts/providers @fuel-ts/script @fuel-ts/signer @fuel-ts/sparsemerkle @fuel-ts/transactions @fuel-ts/versions @fuel-ts/wallet @fuel-ts/wallet-manager @fuel-ts/wordlists"
     deps="$deps $fuel_ts_deps"
 fi
