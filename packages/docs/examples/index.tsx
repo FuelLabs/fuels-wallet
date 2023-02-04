b410bdad3b0f46443d1bc4c9beac334426d39dbf
luizstacio <luizstacio@gmail.com>
2023-02-03 23:54:37 -0300

    docs: add detect fuel section

--- packages/docs/examples/index.tsx
+++ packages/docs/examples/index.tsx
@@ -3,3 +3,4 @@ export { CurrentAccount } from './CurrentAccount';
 export { ListAccounts } from './ListAccounts';
 export { SignMessage } from './SignMessage';
 export { Transfer } from './Transfer';
+export { FuelLoaded } from './FuelLoaded';
