59265ea86d05a540b298388a0cb584433db19ef3
luizstacio <luizstacio@gmail.com>
2023-02-07 23:56:55 -0300

    refactor: clean popup service and add extra check on connection

--- packages/app/src/systems/CRX/background/communication.ts
+++ packages/app/src/systems/CRX/background/communication.ts
@@ -10,6 +10,16 @@ BackgroundService.start(communicationProtocol);
 DatabaseEvents.start(communicationProtocol);
 
 chrome.runtime.onConnect.addListener((port) => {
+  // Only allow connections from the extension
+  // This is to prevent other extensions from connecting
+  // to the background script and sending messages
+  // This is a security measure, this should never happen as
+  // manifest configuration externally_connectable is set to
+  // only allow connections from the extension itself
+  if (port.sender?.id !== chrome.runtime.id) {
+    port.disconnect();
+    return;
+  }
   if (port.name === BACKGROUND_SCRIPT_NAME) {
     communicationProtocol.addConnection(port);
   }
