9948ee0b69fbc974b292f8d1bb9a7b97e88dc6a3
luizstacio <luizstacio@gmail.com>
2023-02-12 19:12:27 -0300

    test: add unlock flow test

---
+++ packages/app/playwright/e2e/UnlockScreen.test.ts
@@ -0,0 +1,42 @@
+import type { Browser, Page } from '@playwright/test';
+import test, { chromium } from '@playwright/test';
+
+import { getByAriaLabel, hasText, reload, visit } from '../commons';
+import { mockData, WALLET_PASSWORD } from '../mocks';
+
+test.describe('UnlockScreen', () => {
+  let browser: Browser;
+  let page: Page;
+
+  test.beforeAll(async () => {
+    browser = await chromium.launch();
+    page = await browser.newPage();
+    await visit(page, '/');
+    await mockData(page);
+  });
+
+  test('UnLock and Lock wallet screen', async () => {
+    await getByAriaLabel(page, 'Menu').click();
+    await page.getByText(/Lock Wallet/i).click();
+    await hasText(page, 'Unlock your wallet to continue');
+    await reload(page);
+    await hasText(page, 'Unlock your wallet to continue');
+    await getByAriaLabel(page, 'Your Password').type(WALLET_PASSWORD);
+    await getByAriaLabel(page, 'Unlock wallet').click();
+    await hasText(page, /assets/i);
+    await reload(page);
+    await hasText(page, /assets/i);
+  });
+
+  test('Reset password on wallet', async () => {
+    // Lock wallet first
+    await getByAriaLabel(page, 'Menu').click();
+    await page.getByText(/Lock Wallet/i).click();
+    await hasText(page, 'Unlock your wallet to continue');
+
+    await page.getByText('Forgot password?').click();
+    await hasText(page, /Reset wallet/i);
+    await getByAriaLabel(page, 'Reset wallet').click();
+    await hasText(page, /Create a new Fuel Wallet/i);
+  });
+});
