970c58c1f257f4ae46fbe921c0f73c2a04b38db6
Pedro Nauck <pedronauck@gmail.com>
2023-03-21 18:01:07 -0300

    fix: codegen script

--- packages/app/scripts/fix-codegen.sh
+++ packages/app/scripts/fix-codegen.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-script_dir="$(dirname "$(realpath "$0")")"
-input_file="$script_dir/../src/generated/graphql.ts"
+input_file="./src/generated/graphql.ts"
 sed -i '' 's|graphql-request/dist/types\.dom|graphql-request/src/types.dom|g' "$input_file"
