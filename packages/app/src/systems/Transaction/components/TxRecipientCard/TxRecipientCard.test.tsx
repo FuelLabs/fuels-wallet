b7a455051f32541a83d7869a857cdd564be61c4c
Eswara Sai <eswarasai.v@gmail.com>
2023-03-28 17:29:23 +0530

    chore: added account name in balance widget

--- packages/app/src/systems/Transaction/components/TxRecipientCard/TxRecipientCard.test.tsx
+++ packages/app/src/systems/Transaction/components/TxRecipientCard/TxRecipientCard.test.tsx
@@ -1,14 +1,19 @@
-import { render, screen, testA11y } from '@fuel-ui/test-utils';
+import { screen, testA11y } from '@fuel-ui/test-utils';
 
 import { MOCK_TX_RECIPIENT } from '../../__mocks__/tx-recipient';
 
 import { TxRecipientCard } from './TxRecipientCard';
 
+import { TestWrapper } from '~/systems/Core';
+import { renderWithProvider } from '~/systems/Core/__tests__';
+
 const { account: ACCOUNT, contract: CONTRACT } = MOCK_TX_RECIPIENT;
 
 describe('TxRecipientCard', () => {
   it('a11y', async () => {
-    await testA11y(<TxRecipientCard recipient={ACCOUNT} />);
+    await testA11y(<TxRecipientCard recipient={ACCOUNT} />, {
+      wrapper: TestWrapper,
+    });
   });
 
   it('a11yLoader ', async () => {
@@ -16,18 +21,18 @@ describe('TxRecipientCard', () => {
   });
 
   it('should render account correctly', () => {
-    render(<TxRecipientCard recipient={ACCOUNT} />);
+    renderWithProvider(<TxRecipientCard recipient={ACCOUNT} />);
     expect(screen.getByText('fuel1g...kuj7')).toBeInTheDocument();
   });
 
   it('should render contract correctly', () => {
-    render(<TxRecipientCard recipient={CONTRACT} />);
+    renderWithProvider(<TxRecipientCard recipient={CONTRACT} />);
     expect(screen.getByText('From (Contract)')).toBeInTheDocument();
     expect(screen.getByText('fuel1y...y6wk')).toBeInTheDocument();
   });
 
   it('should change title with isReceiver prop', () => {
-    render(<TxRecipientCard recipient={CONTRACT} isReceiver />);
+    renderWithProvider(<TxRecipientCard recipient={CONTRACT} isReceiver />);
     expect(screen.getByText('To (Contract)')).toBeInTheDocument();
   });
 });
