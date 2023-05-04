103a66bebbf7f24a8291ac84ab5bf4a447320188
Eswara Sai <eswarasai.v@gmail.com>
2023-05-04 21:02:35 +0530

    fix: added localhost to url provider validation

--- packages/app/src/systems/Network/hooks/useNetworkForm.ts
+++ packages/app/src/systems/Network/hooks/useNetworkForm.ts
@@ -15,9 +15,10 @@ function isValidUrl(url?: string) {
   if (!url) return false;
   // Note: new URL('https://graphql') returns `true`
   const pattern = new RegExp(
-    '^(https?:\\/\\/)?' +
+    '^(https?:\\/\\/)' +
       '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
-      '((\\d{1,3}\\.){3}\\d{1,3}))' +
+      '((\\d{1,3}\\.){3}\\d{1,3}))|' +
+      'localhost' +
       '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
       '(\\?[;&a-z\\d%_.~+=-]*)?' +
       '(\\#[-a-z\\d_]*)?$',
