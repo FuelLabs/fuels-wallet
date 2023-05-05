e139e6972cfa6a144c45c01137bc4f3a17b4c898
tomide <ayotomiiide@gmail.com>
2023-05-05 18:12:28 +0100

    refactor: remove addAccount functionality

--- packages/app/src/systems/Account/hooks/index.tsx
+++ packages/app/src/systems/Account/hooks/index.tsx
@@ -1,6 +1,5 @@
 export * from './useAccounts';
 export * from './useAccountForm';
-export * from './useAddAccount';
 export * from './useEditAccount';
 export * from './useImportAccount';
 export * from './useImportAccountForm';
