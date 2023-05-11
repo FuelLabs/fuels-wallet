8ec032c76644cd4f5523021ffe57bcfaf917ffa2
Tiberiu Lepadatu <tiberiulepadatu14@gmail.com>
2023-05-11 16:38:29 -0600

    add test for mnemonic

--- packages/app/playwright/e2e/RecoverWallet.test.ts
+++ packages/app/playwright/e2e/RecoverWallet.test.ts
@@ -55,6 +55,48 @@ test.describe('RecoverWallet', () => {
     await hasText(page, 'fuel1r...xqqj');
   });
 
+  test('should auto-select right mnemonic format and recover wallet', async () => {
+    await visit(page, '/wallet');
+    await logout(page);
+    await getButtonByText(page, /I already have a wallet/i).click();
+
+    /** Accept terms and conditions */
+    await hasText(page, /Terms of service/i);
+    await getButtonByText(page, /I accept/i).click();
+
+    /** Select the wrong mnemonic size */
+    await getByAriaLabel(page, 'Select format').selectOption(
+      'I have a 12 words seed phrase'
+    );
+
+    /** Copy words to clipboard area */
+    await page.evaluate(`navigator.clipboard.writeText('${WORDS_24}')`);
+
+    /** Simulating clipboard write */
+    await getButtonByText(page, /Paste/i).click();
+
+    /** Confirm Mnemonic */
+    await hasText(page, /Enter your Recovery Phrase/i);
+    await getButtonByText(page, /Paste/i).click();
+    await getButtonByText(page, /Next/i).click();
+
+    /** Adding password */
+    await hasText(page, /Encrypt your wallet/i);
+    const passwordInput = await getByAriaLabel(page, 'Your Password');
+    await passwordInput.type(WALLET_PASSWORD);
+    await passwordInput.press('Tab');
+    const confirmPasswordInput = await getByAriaLabel(page, 'Confirm Password');
+    await confirmPasswordInput.type(WALLET_PASSWORD);
+    await confirmPasswordInput.press('Tab');
+
+    await getButtonByText(page, /Next/i).click();
+
+    /** Account created */
+    await hasText(page, /Wallet created successfully/i);
+    await hasText(page, /Account 1/i);
+    await hasText(page, 'fuel1w...4rtl');
+  });
+
   test('should be able to recover a wallet from 24-word mnemonic', async () => {
     await visit(page, '/wallet');
     await logout(page);
