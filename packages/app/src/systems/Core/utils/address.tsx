0984defa7482c8406972fffe91b7cd7cb4ca2551
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2023-06-05 19:11:32 -0300

    fix

--- packages/app/src/systems/Core/utils/address.tsx
+++ packages/app/src/systems/Core/utils/address.tsx
@@ -1,3 +1,5 @@
+import { isB256 } from 'fuels';
+
 export function shortAddress(address: string = '') {
   return address.length > 10
     ? `${address.slice(0, 6)}...${address.slice(-4)}`
@@ -5,5 +7,5 @@ export function shortAddress(address: string = '') {
 }
 
 export function isValidEthAddress(address: string = '') {
-  return /^0x[a-fA-F0-9]{40}$/.test(address);
+  return isB256(address) || /^0x[a-fA-F0-9]{40}$/.test(address);
 }
