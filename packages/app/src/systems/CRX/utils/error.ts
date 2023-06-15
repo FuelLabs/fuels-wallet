6f7789d17bf37e29a603f2d7ab706e13c52eb1de
tomide <ayotomiiide@gmail.com>
2023-06-15 20:14:46 +0100

    refactor: fix small issues

--- packages/app/src/systems/CRX/utils/error.ts
+++ packages/app/src/systems/CRX/utils/error.ts
@@ -1,10 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function errorBoundary<T extends () => any>(cb: T): ReturnType<T> {
+  // eslint-disable-next-line no-useless-catch
   try {
     return cb();
   } catch (err) {
-    // eslint-disable-next-line no-console
-    console.log(err);
     throw err;
   }
 }
