66fc11eb2f681aab11aa7d1337c2e647f7ba90b7
Luiz Estácio | stacio.eth <luizstacio@gmail.com>
2023-07-06 05:26:15 +0100

    feat: add error reporting tool using sentry (#789)

--- packages/app/src/main.tsx
+++ packages/app/src/main.tsx
@@ -2,8 +2,8 @@ import '@fontsource/source-code-pro';
 import { createRoot } from 'react-dom/client';
 
 import { App } from './App';
-import { RouterProvider } from './systems/Core';
 import './exports';
+import { RouterProvider } from './systems/Core/components/RouterProvider';
 
 createRoot(document.getElementById('root')!).render(
   <RouterProvider>
