afb0ea0de06f12d4d4e12b02a30972991fe13076
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2023-07-31 16:58:19 -0300

    fix: prettier changes

--- packages/app/src/systems/Settings/pages/ChangePassword/ChangePassword.tsx
+++ packages/app/src/systems/Settings/pages/ChangePassword/ChangePassword.tsx
@@ -64,7 +64,7 @@ export function ChangePassword() {
     debounce(() => {
       trigger('confirmPassword');
     }, 500),
-    [],
+    []
   );
 
   useEffect(() => {
--- packages/app/src/systems/SignUp/components/CreatePassword/CreatePassword.tsx
+++ packages/app/src/systems/SignUp/components/CreatePassword/CreatePassword.tsx
@@ -74,7 +74,7 @@ export function CreatePassword({
     debounce(() => {
       trigger('confirmPassword');
     }, 500),
-    [],
+    []
   );
 
   return (
