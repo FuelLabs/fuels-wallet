fb9f042c5bdbff23213cb000b79e7353bc506456
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2023-08-01 14:43:21 -0300

    fix: network name

--- packages/app/playwright/e2e/Networks.test.ts
+++ packages/app/playwright/e2e/Networks.test.ts
@@ -89,12 +89,12 @@ test.describe('Networks', () => {
     const urlInput = getInputByName(page, 'url');
     await expect(urlInput).toBeFocused();
     await urlInput.fill(VITE_FUEL_PROVIDER_URL);
-    await hasText(page, /Testnet Beta 3/i, 0, 15000);
+    await hasText(page, /Testnet/i, 0, 15000);
     await expect(buttonCreate).toBeEnabled();
     await buttonCreate.click();
     // Wait for save and close popup;
     await page.waitForTimeout(2000);
     await reload(page);
-    await hasText(page, /Testnet Beta 3/i);
+    await hasText(page, /Testnet/i);
   });
 });
