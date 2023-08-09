e596d279a1d5f4e4961bbda3e3136afceab0b63a
Pedro Nauck <pedronauck@gmail.com>
2023-08-09 16:41:32 -0300

    fix: builds

--- scripts/icons.sh
+++
@@ -1,4 +0,0 @@
-PNPM_STORE=$(pnpm root)
-
-cp "${PNPM_STORE}/@fuel-ui/icons/dist/icons/sprite.svg" './packages/app/public/icons'
-cp "${PNPM_STORE}/@fuel-ui/icons/dist/icons/sprite.svg" './packages/docs/public/icons'
