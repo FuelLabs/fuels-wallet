44176cd4251263b3b01c5cf29a29588827eebd37
Pedro Nauck <pedronauck@gmail.com>
2023-08-09 15:55:57 -0300

    fix: storybook build

--- packages/app/.storybook/preview.tsx
+++ packages/app/.storybook/preview.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import {
-  darkTheme,
-  lightTheme,
-  loadIcons,
-  setFuelThemes,
-  ThemeProvider,
-} from '@fuel-ui/react';
+import { darkTheme, lightTheme } from '@fuel-ui/react';
 import { themes } from '@storybook/theming';
 import { mswDecorator, initialize } from 'msw-storybook-addon';
 import { withRouter } from 'storybook-addon-react-router-v6';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import icons from '~icons-url';
+import { Providers } from '../src/systems/Core/components';
 
 import { MINIMAL_VIEWPORTS } from '@storybook/addon-viewport';
-import { StoreProvider } from '../src/systems/Store';
 import { WALLET_WIDTH, WALLET_HEIGHT } from '../src/config';
 
 import theme from './theme';
@@ -64,23 +55,13 @@ export const parameters = {
   },
 };
 
-loadIcons(icons);
-setFuelThemes({
-  themes: {
-    dark: darkTheme,
-    light: lightTheme,
-  },
-});
-
 export const decorators = [
   mswDecorator,
   withRouter,
   (Story: any) => (
-    <StoreProvider>
-      <ThemeProvider>
-        <Story />
-      </ThemeProvider>
-    </StoreProvider>
+    <Providers>
+      <Story />
+    </Providers>
   ),
 ];
 
