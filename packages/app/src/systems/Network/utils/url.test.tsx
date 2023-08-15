a6ad098a7218b01f382bfc69f55384ecfead8511
Luiz Estácio <luizstacio@gmail.com>
2023-08-14 23:00:06 -0300

    chore: fix dev url

--- packages/app/src/systems/Network/utils/url.test.tsx
+++ packages/app/src/systems/Network/utils/url.test.tsx
@@ -3,7 +3,7 @@ import { isValidNetworkUrl } from './url';
 describe('isValidNetworkUrl()', () => {
   it('should return true for valid network urls', () => {
     expect(isValidNetworkUrl('http://localhost:4000/graphql')).toBe(true);
-    expect(isValidNetworkUrl('https://beta3-5-devv.swayswap.io/graphql')).toBe(
+    expect(isValidNetworkUrl('https://beta3-5-dev.swayswap.io/graphql')).toBe(
       true
     );
   });
@@ -11,7 +11,7 @@ describe('isValidNetworkUrl()', () => {
   it('should return false for invalid network urls', () => {
     expect(isValidNetworkUrl('http://localhost:4000')).toBe(false);
     expect(isValidNetworkUrl('localhost:4000/graphql')).toBe(false);
-    expect(isValidNetworkUrl('https://beta3-5-devv.swayswap.io')).toBe(false);
+    expect(isValidNetworkUrl('https://beta3-5-dev.swayswap.io')).toBe(false);
     expect(isValidNetworkUrl('beta3-5-devv.swayswap.io/graphql')).toBe(false);
   });
 });
