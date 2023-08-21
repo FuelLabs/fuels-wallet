d0fc8ef8e066a65bcff8509eaf3218813453eafc
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2023-08-21 00:26:27 -0300

    chore: try fix e2e

--- packages/app/playwright/commons/gas.ts
+++ packages/app/playwright/commons/gas.ts
@@ -1,8 +1,10 @@
 import type { Provider } from 'fuels';
 
 export const getGasConfig = async (provider: Provider) => {
-  const gasLimit = (await provider.getChain()).consensusParameters.maxGasPerTx;
-  const gasPrice = (await provider.getNodeInfo()).minGasPrice;
+  const chain = await provider.getChain();
+  const nodeInfo = await provider.getNodeInfo();
+  const gasLimit = chain.consensusParameters.maxGasPerTx;
+  const gasPrice = nodeInfo.minGasPrice;
 
   return { gasLimit, gasPrice };
 };
