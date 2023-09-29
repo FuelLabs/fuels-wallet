4e78a20ce6d33fee1ba4f44599b89650a61c7a3b
Matt Auer <mattauer@umich.edu>
2023-09-29 14:37:09 -0500

    chore: generate ts types for contracts

--- packages/e2e-contract-tests/src/App.tsx
+++ packages/e2e-contract-tests/src/App.tsx
@@ -1,14 +1,32 @@
-import { Nav } from '@fuel-ui/react';
+import { Box, Button, Card, Nav, ThemeProvider } from '@fuel-ui/react';
 
 function App() {
   return (
-    <>
+    <ThemeProvider>
       <Nav>
         <Nav.Desktop>
           <Nav.Logo />
         </Nav.Desktop>
       </Nav>
-    </>
+      <Box.Flex justify="space-around" css={{ marginTop: '40px' }}>
+        <Card>
+          <Card.Header>Mint Custom Asset</Card.Header>
+          <Card.Body>
+            <Button onPress={() => {}} css={{ width: '$full' }}>
+              Mint
+            </Button>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Header>Mint Custom Asset</Card.Header>
+          <Card.Body>Hello</Card.Body>
+        </Card>
+        <Card>
+          <Card.Header>Mint Custom Asset</Card.Header>
+          <Card.Body>Hello</Card.Body>
+        </Card>
+      </Box.Flex>
+    </ThemeProvider>
   );
 }
 
