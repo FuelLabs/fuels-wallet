3830d61e6feb83eb21a613e747521436336a60a9
Matt Auer <mattauer@umich.edu>
2023-09-29 10:34:08 -0500

    feat: basic frontend setup

---
+++ packages/e2e-contract-tests/src/App.tsx
@@ -0,0 +1,15 @@
+import { Nav } from '@fuel-ui/react';
+
+function App() {
+  return (
+    <>
+      <Nav>
+        <Nav.Desktop>
+          <Nav.Logo />
+        </Nav.Desktop>
+      </Nav>
+    </>
+  );
+}
+
+export default App;
