6277bb236d82cdfb483a06c1d82d9668ee367c7c
Matt Auer <mattauer@umich.edu>
2023-10-04 10:38:36 -0500

    refactor: remove unnecessary env definitions

--- packages/e2e-contract-tests/src/vite-env.d.ts
+++ packages/e2e-contract-tests/src/vite-env.d.ts
@@ -7,15 +7,3 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
-
-// Add script module importing (ECMAScript)
-declare module '*?script&module' {
-  const src: string;
-  export default src;
-}
-
-// Add script  importing (CommonJS)
-declare module '*?script' {
-  const src: string;
-  export default src;
-}
