b624c6ddb3b6965fc6646525855a9f5dbb3cc7a8
Matt Auer <mattauer@umich.edu>
2023-10-05 12:57:19 -0500

    feat: create depsit half component

--- packages/e2e-contract-tests/src/App.tsx
+++ packages/e2e-contract-tests/src/App.tsx
@@ -1,4 +1,5 @@
 import { Header, MintAssetCard, Providers } from './components';
+import { DepositHalfEthCard } from './components/DepositHalfEthCard';
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <Header />
       <div>
         <MintAssetCard />
+        <DepositHalfEthCard />
       </div>
     </Providers>
   );
