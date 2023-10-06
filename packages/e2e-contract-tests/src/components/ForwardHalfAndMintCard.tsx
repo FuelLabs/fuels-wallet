a1f1cba543a6974daaadc07425c293e1f4a99f5d
Matt Auer <mattauer@umich.edu>
2023-10-06 14:28:09 -0500

    feat: forward half and mint card

---
+++ packages/e2e-contract-tests/src/components/ForwardHalfAndMintCard.tsx
@@ -0,0 +1,44 @@
+import { useAccount, useWallet } from '@fuel-wallet/react';
+import { BaseAssetId, bn } from 'fuels';
+import { useState } from 'react';
+
+import { depositHalfAndMint } from '../contract_interactions';
+
+export const ForwardHalfAndMintCard = () => {
+  const [forwardAmount, setForwardAmount] = useState<string>('');
+  const [mintAmount, setMintAmount] = useState<string>('');
+  const account = useAccount();
+  const wallet = useWallet({ address: account.account });
+
+  return (
+    <div>
+      <p>Forward Custom Asset</p>
+      <div aria-label="forward half and mint card">
+        <input
+          aria-label="Forward amount"
+          onChange={(event) => setForwardAmount(event.target.value)}
+          value={forwardAmount}
+        />
+        <input
+          aria-label="Mint amount"
+          onChange={(event) => setMintAmount(event.target.value)}
+          value={mintAmount}
+        />
+        <button
+          onClick={async () => {
+            if (wallet.wallet && mintAmount && forwardAmount) {
+              await depositHalfAndMint({
+                wallet: wallet.wallet,
+                forwardAmount: bn.parseUnits(forwardAmount),
+                mintAmount: bn.parseUnits(mintAmount),
+                assetId: BaseAssetId,
+              });
+            }
+          }}
+        >
+          Forward Half And Mint
+        </button>
+      </div>
+    </div>
+  );
+};
