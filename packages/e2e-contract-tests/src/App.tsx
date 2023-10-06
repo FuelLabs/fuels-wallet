7ba00594cb1bec7d1b1200a32efeca2b73e7099b
Matt <54373384+matt-user@users.noreply.github.com>
2023-10-06 18:16:00 -0400

    test: e2e contract test forward eth and mint mint (#902)
    closes FRO-605

--- packages/e2e-contract-tests/src/App.tsx
+++ packages/e2e-contract-tests/src/App.tsx
@@ -5,6 +5,7 @@ import {
   Providers,
   DepositHalfEthCard,
   ForwardCustomAssetCard,
+  ForwardHalfAndMintCard,
 } from './components';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <ForwardEthCard />
         <DepositHalfEthCard />
         <ForwardCustomAssetCard />
+        <ForwardHalfAndMintCard />
       </div>
     </Providers>
   );
