03dfa6acddb4f9fa2986f08018a2532659a567c2
Matt Auer <mattauer@umich.edu>
2023-10-30 17:08:13 -0500

    feat: add download fuel

---
+++ packages/test-utils/src/fixtures/fixtures.ts
@@ -0,0 +1,31 @@
+// Use a test fixture to set the context so tests have access to the wallet extension.
+import { test as base, chromium, type BrowserContext } from '@playwright/test';
+import path from 'path';
+
+export const test = base.extend<{
+  context: BrowserContext;
+  extensionId: string;
+  pathToExtension: string;
+}>({
+  pathToExtension: path.join(__dirname, '../../../app/dist-crx'),
+  context: async ({ pathToExtension }, use) => {
+    const context = await chromium.launchPersistentContext('', {
+      headless: false,
+      args: [
+        `--disable-extensions-except=${pathToExtension}`,
+        `--load-extension=${pathToExtension}`,
+      ],
+    });
+    await use(context);
+    await context.close();
+  },
+  extensionId: async ({ context }, use) => {
+    let [background] = context.serviceWorkers();
+    if (!background) background = await context.waitForEvent('serviceworker');
+
+    const extensionId = background.url().split('/')[2];
+    await use(extensionId);
+  },
+});
+
+export const expect = test.expect;
