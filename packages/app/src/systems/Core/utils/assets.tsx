84b2ce79358f5bebfafa485cedafb8750cbd1de4
Matt Auer <mattauer@umich.edu>
2024-01-17 10:38:29 -0600

    chore: remove comments

--- packages/app/src/systems/Core/utils/assets.tsx
+++ packages/app/src/systems/Core/utils/assets.tsx
@@ -1,17 +1,6 @@
 import type { AssetFuel } from '@fuels/assets';
 import assets, { getAssetFuel } from '@fuels/assets';
 
-// export const fuelAssets = assets.map((asset) => {
-//   const fuelNetworkAsset = asset.networks.find(
-//     (n) => n.type === 'fuel'
-//   ) as Fuel;
-//   return {
-//     ...asset,
-//     assetId: fuelNetworkAsset.assetId,
-//     decimals: fuelNetworkAsset.decimals,
-//   };
-// });
-
 export const fuelAssets = assets.reduce((arr, asset) => {
   const fuelAsset = getAssetFuel(asset);
   if (fuelAsset) {
