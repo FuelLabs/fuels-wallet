d4543bcfa22c7722433b045503e9007d50b2cae7
Matt Auer <mattauer@umich.edu>
2024-01-24 16:24:19 -0600

    fix: get jest to work

--- packages/sdk-v2/jest.setup.ts
+++ packages/sdk-v2/jest.setup.ts
@@ -1,9 +1,31 @@
-import { TextEncoder, TextDecoder } from 'util';
+/* eslint-disable import/order */
+import { performance } from 'node:perf_hooks';
+import { clearImmediate } from 'node:timers';
+import { TextDecoder, TextEncoder } from 'node:util';
+import { ReadableStream } from 'web-streams-polyfill';
 
 // https://stackoverflow.com/a/68468204
-Object.assign(global, { TextDecoder, TextEncoder });
+Object.defineProperties(globalThis, {
+  TextDecoder: { value: TextDecoder },
+  TextEncoder: { value: TextEncoder },
+  performance: { value: performance },
+  clearImmediate: { value: clearImmediate },
+  ReadableStream: { value: ReadableStream },
+});
 
-// This is needed bc of this error https://github.com/jestjs/jest/issues/2549
+import { Blob, File } from 'node:buffer';
+import { fetch, Headers, FormData, Request, Response } from 'undici';
+
+Object.defineProperties(globalThis, {
+  fetch: { value: fetch, writable: true },
+  Blob: { value: Blob },
+  File: { value: File },
+  Headers: { value: Headers },
+  FormData: { value: FormData },
+  Request: { value: Request },
+  Response: { value: Response },
+});
+
 Object.defineProperty(Uint8Array, Symbol.hasInstance, {
   value(potentialInstance: unknown) {
     return this === Uint8Array
---
+++ packages/sdk-v2/type.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="node" />
+
+declare module 'blob-polyfill';
