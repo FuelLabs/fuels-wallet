02fd9e7c28e62fc951092a52df707052fb4f7e68
Hélcio Franco <helciofranco@me.com>
2024-03-14 15:40:27 -0400

    feat: add @fuels/connectors to the dapp

--- examples/cra-dapp/src/main.tsx
+++ examples/cra-dapp/src/main.tsx
@@ -1,3 +1,8 @@
+import {
+  FuelWalletConnector,
+  FuelWalletDevelopmentConnector,
+  FueletWalletConnector,
+} from '@fuels/connectors';
 import { FuelProvider } from '@fuels/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -6,7 +11,15 @@ import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <FuelProvider>
+    <FuelProvider
+      fuelConfig={{
+        connectors: [
+          new FuelWalletConnector(),
+          new FuelWalletDevelopmentConnector(),
+          new FueletWalletConnector(),
+        ],
+      }}
+    >
       <App />
     </FuelProvider>
   </React.StrictMode>
