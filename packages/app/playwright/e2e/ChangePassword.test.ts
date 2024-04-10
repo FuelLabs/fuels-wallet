8f94aee7b105fa426655689cb6c8761a09ac23ed
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2024-04-10 09:53:52 -0300

    fix: Change Password dialog allowing new password to be the same as current #1203 (#1217)
    Fixes #1203 
    - Prevents change password dialog from allowing the new password from
    being the same as the current password
    
    
    
    
    | Before     | After |
    | ---      | ---       |
    | <img width="348" alt="Screenshot 2024-04-08 at 20 27 43"
    src="https://github.com/FuelLabs/fuels-wallet/assets/3487334/18fd7bb8-a52a-4d03-ac58-94a01ccf72c2">
    | <img width="345" alt="Screenshot 2024-04-08 at 20 26 12"
    src="https://github.com/FuelLabs/fuels-wallet/assets/3487334/969dae7f-72ac-4d67-8a5b-699d91a65fa7">
    |

--- packages/app/playwright/e2e/ChangePassword.test.ts
+++ packages/app/playwright/e2e/ChangePassword.test.ts
@@ -66,4 +66,23 @@ test.describe('ChangePassword', () => {
 
     await hasText(page, 'Passwords must match');
   });
+
+  test('should not change the user current and new passwords are the same', async () => {
+    // goes to the change password page
+    await visit(page, '/settings/change-password');
+
+    // ensure that the page has changed
+    await hasText(page, /Change Password/i);
+
+    // fills form data
+    await getByAriaLabel(page, 'Current Password').fill('newPass12345$');
+    await getByAriaLabel(page, 'New Password').fill('newPass12345$');
+    await getByAriaLabel(page, 'Confirm Password').fill('newPass12345$');
+    await getByAriaLabel(page, 'Confirm Password').blur();
+
+    await hasText(
+      page,
+      'New password cannot be the same as the current password'
+    );
+  });
 });
