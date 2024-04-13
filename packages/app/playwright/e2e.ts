e11d22d27e01e565590da3af94472cd8de7ee684
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2024-04-13 18:39:32 -0300

    chore: use real connectors

--- packages/app/playwright/e2e.ts
+++ packages/app/playwright/e2e.ts
@@ -1,10 +1,10 @@
+import { defaultConnectors } from '@fuels/connectors';
 import { Address, Fuel, type StorageAbstract } from 'fuels';
-import { MockConnector } from './mocks/connector';
 
 window.fuel = new Fuel({
-  connectors: [new MockConnector()],
+  connectors: defaultConnectors(),
   storage: {
-    getItem: () => Promise.resolve('Fuel Wallet'),
+    getItem: () => Promise.resolve(undefined),
     setItem: () => Promise.resolve(),
     removeItem: () => Promise.resolve(),
     clear: () => Promise.resolve(),
