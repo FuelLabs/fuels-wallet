cfd39235836396ecd77dfb882817f049d2f4485f
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2024-04-13 18:55:59 -0300

    chore: remove storage mock

--- packages/app/playwright/e2e.ts
+++ packages/app/playwright/e2e.ts
@@ -1,14 +1,10 @@
 import { defaultConnectors } from '@fuels/connectors';
 import { Address, Fuel, type StorageAbstract } from 'fuels';
 
+localStorage.clear();
+
 window.fuel = new Fuel({
   connectors: defaultConnectors(),
-  storage: {
-    getItem: () => Promise.resolve(undefined),
-    setItem: () => Promise.resolve(),
-    removeItem: () => Promise.resolve(),
-    clear: () => Promise.resolve(),
-  } as StorageAbstract,
 });
 
 window.createAddress = (address: string) => Address.fromString(address);
