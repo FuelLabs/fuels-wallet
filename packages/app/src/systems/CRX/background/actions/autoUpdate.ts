700543332468f935f6f761ef9b010058c6eb7369
luizstacio <luizstacio@gmail.com>
2024-04-22 18:54:32 -0300

    chore: change version api

--- packages/app/src/systems/CRX/background/actions/autoUpdate.ts
+++ packages/app/src/systems/CRX/background/actions/autoUpdate.ts
@@ -1,14 +1,15 @@
 /* eslint-disable no-console */
 import { compareVersions } from 'compare-versions';
-import { APP_VERSION, VITE_CRX_VERSION_API } from '~/config';
+import { APP_VERSION, VITE_CRX_VERSION_API, WALLET_NAME } from '~/config';
 
 async function runVersionCheck() {
-  const { version } = await fetch(VITE_CRX_VERSION_API)
+  const latestVersion = await fetch(VITE_CRX_VERSION_API)
     .then((res) => res.json())
     // If fails to fetch the version return current version
     .catch(() => ({ version: APP_VERSION }));
+  const version = latestVersion[WALLET_NAME];
   // If app version is greater than the one on the release API ignores the check
-  if (compareVersions(APP_VERSION, version) < 0) return;
+  if (compareVersions(APP_VERSION, version) > -1) return;
   // Request update check and reload if available
   console.log('[FUEL WALLET] Checking for updates...');
   chrome.runtime.requestUpdateCheck((details) => {
