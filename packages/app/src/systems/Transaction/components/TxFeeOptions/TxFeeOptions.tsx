1ba870ca380f52c02612715626b59aa12cb5e6a0
Hélcio Franco <helciofranco@me.com>
2024-04-29 18:00:21 -0400

    feat: add transitions between advanced and regular fees options

--- packages/app/src/systems/Transaction/components/TxFeeOptions/TxFeeOptions.tsx
+++ packages/app/src/systems/Transaction/components/TxFeeOptions/TxFeeOptions.tsx
@@ -1,5 +1,7 @@
-import { Box } from '@fuel-ui/react';
+import { Box, Button } from '@fuel-ui/react';
+import { AnimatePresence } from 'framer-motion';
 import type { BN } from 'fuels';
+import { MotionFlex, MotionStack, animations } from '~/systems/Core';
 import type { FeeType } from '~/systems/Send/hooks';
 import { TxFee } from '../TxFee/TxFee';
 
@@ -18,30 +20,61 @@ export const TxFeeOptions = ({
 }: TxFeeOptionsProps) => {
   const isAdvanced = currentFeeType === 'advanced';
 
+  const toggle = () => {
+    if (isAdvanced) {
+      onChangeCurrentFeeType('regular');
+      return;
+    }
+
+    onChangeCurrentFeeType('advanced');
+  };
+
   return (
     <Box.Stack gap="$1">
-      {!isAdvanced && (
-        <>
-          <TxFee
-            fee={regularFee}
-            title="Regular"
-            checked={currentFeeType === 'regular'}
-            onChecked={() => onChangeCurrentFeeType('regular')}
-          />
-          <TxFee
-            fee={fastFee}
-            title="Fast"
-            checked={currentFeeType === 'fast'}
-            onChecked={() => onChangeCurrentFeeType('fast')}
-          />
-        </>
-      )}
-      <TxFee
-        fee={undefined}
-        title="Advanced"
-        checked={isAdvanced}
-        onChecked={() => onChangeCurrentFeeType('advanced')}
-      />
+      <AnimatePresence mode="popLayout">
+        {isAdvanced ? (
+          <MotionStack
+            {...animations.slideInTop()}
+            key="advanced"
+            gap="$3"
+            layout
+          >
+            @TODO: Add tip and gas limit fields here
+          </MotionStack>
+        ) : (
+          <MotionStack
+            {...animations.slideInTop()}
+            key="regular"
+            gap="$3"
+            layout
+          >
+            <TxFee
+              fee={regularFee}
+              title="Regular"
+              checked={currentFeeType === 'regular'}
+              onChecked={() => onChangeCurrentFeeType('regular')}
+            />
+            <TxFee
+              fee={fastFee}
+              title="Fast"
+              checked={currentFeeType === 'fast'}
+              onChecked={() => onChangeCurrentFeeType('fast')}
+            />
+          </MotionStack>
+        )}
+
+        <MotionFlex
+          {...animations.fadeIn()}
+          key="toggle"
+          align="center"
+          direction="column"
+          layout
+        >
+          <Button size="xs" variant="link" onPress={toggle}>
+            {isAdvanced ? 'Hide' : 'Show'} advanced options
+          </Button>
+        </MotionFlex>
+      </AnimatePresence>
     </Box.Stack>
   );
 };
