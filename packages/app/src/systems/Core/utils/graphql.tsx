3883ad09f44e0eadfec5b34930339a7e182c8c9f
Hélcio Franco <helciofranco@me.com>
2024-05-07 20:32:13 -0400

    fix: remove codegen legacy code

--- packages/app/src/systems/Core/utils/graphql.tsx
+++
@@ -1,7 +0,0 @@
-import { GraphQLClient } from 'graphql-request';
-import { getSdk } from '~/generated/graphql';
-
-export function getGraphqlClient(providerUrl: string) {
-  const graphqlClient = new GraphQLClient(providerUrl);
-  return getSdk(graphqlClient);
-}
