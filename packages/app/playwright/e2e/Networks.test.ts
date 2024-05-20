6be633f8e62d95d3551c57915b6f48737e8c23e6
LuizAsFight <felipebolsonigomes@gmail.com>
2024-05-21 00:52:53 +0200

    fix: test

--- packages/app/playwright/e2e/Networks.test.ts
+++ packages/app/playwright/e2e/Networks.test.ts
@@ -89,12 +89,12 @@ test.describe('Networks', () => {
     const urlInput = getInputByName(page, 'url');
     await expect(urlInput).toBeFocused();
     await urlInput.fill('https://testnet.fuel.network/v1/graphql');
-    await hasText(page, /Ignition\-Dev/i, 0, 15000);
+    await hasText(page, /Ignition/i, 0, 15000);
     await expect(buttonCreate).toBeEnabled();
     await buttonCreate.click();
     // Wait for save and close popup;
     await page.waitForTimeout(2000);
     await reload(page);
-    await hasText(page, /Ignition\-Dev/i);
+    await hasText(page, /Ignition/i);
   });
 });
