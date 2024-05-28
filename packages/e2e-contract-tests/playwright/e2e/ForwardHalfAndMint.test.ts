641775b37cca15224f439b7e77dbfa2a1592c484
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2024-05-28 16:22:13 -0300

    fix: tests downloading production wallet instead of using the build

--- packages/e2e-contract-tests/playwright/e2e/ForwardHalfAndMint.test.ts
+++ packages/e2e-contract-tests/playwright/e2e/ForwardHalfAndMint.test.ts
@@ -13,8 +13,10 @@ import {
 import { testSetup } from '../utils';
 
 import { MAIN_CONTRACT_ID } from './config';
-import { test } from './test';
+import { test, useLocalCRX } from './test';
 import { checkAddresses, checkFee, connect } from './utils';
+
+useLocalCRX();
 
 test.describe('Forward Half ETH and Mint Custom Asset', () => {
   let fuelWalletTestHelper: FuelWalletTestHelper;
--- packages/e2e-contract-tests/playwright/e2e/test.ts
+++ packages/e2e-contract-tests/playwright/e2e/test.ts
@@ -1,8 +1,12 @@
 import { join } from 'node:path';
 import { test as testConfig } from '@fuels/playwright-utils';
 
-testConfig.use({
-  pathToExtension: join(__dirname, '../../../app/dist-crx'),
-});
+// This must be called so that the local crx is used, otherwise a production build will be downloaded and used
+// This must be called once for each test file
+export function useLocalCRX() {
+  testConfig.use({
+    pathToExtension: join(__dirname, '../../../app/dist-crx'),
+  });
+}
 
 export const test = testConfig;
