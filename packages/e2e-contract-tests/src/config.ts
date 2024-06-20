3ae1bdaf68a95d74d45f205dc54b0f9613a1e5b0
Luiz Gomes <8636507+LuizAsFight@users.noreply.github.com>
2024-06-20 00:28:09 -0300

    Update packages/e2e-contract-tests/src/config.ts
    Co-authored-by: Hélcio Franco <helciofranco@me.com>

--- packages/e2e-contract-tests/src/config.ts
+++ packages/e2e-contract-tests/src/config.ts
@@ -7,12 +7,12 @@ const FUEL_PROVIDER_URL = import.meta.env.VITE_FUEL_PROVIDER_URL;
 let contractsIdsToUse: Record<string, string> | undefined;
 
 if (FUEL_PROVIDER_URL?.indexOf('testnet') !== -1) {
-  constracsIdsToUse = contractIdsTestnet;
+  contractsIdsToUse = contractIdsTestnet;
 } else if (FUEL_PROVIDER_URL?.indexOf('devnet') !== -1) {
-  constracsIdsToUse = contractIdsDevnet;
+  contractsIdsToUse = contractIdsDevnet;
 } else {
-  constracsIdsToUse = contractIds;
+  contractsIdsToUse = contractIds;
 }
 
-export const MAIN_CONTRACT_ID = constracsIdsToUse.MainContract;
-export const EXTERNAL_CONTRACT_ID = constracsIdsToUse.ExternalContract;
+export const MAIN_CONTRACT_ID = contractsIdsToUse.MainContract;
+export const EXTERNAL_CONTRACT_ID = contractsIdsToUse.ExternalContract;
