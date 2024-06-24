ecd7a2a5ca88f15946e9a1a44f771cf7dc26837c
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2024-06-24 12:26:21 -0300

    feat: show different url depending on current environment

--- packages/docs/src/components/DownloadWalletPreview.tsx
+++ packages/docs/src/components/DownloadWalletPreview.tsx
@@ -1,11 +1,25 @@
-import { WALLET_LINK_NEXT, WALLET_LINK_STAGING } from '../constants';
+import { useContext } from 'react';
+import {
+  DomainContext,
+  WALLET_DOWNLOAD_PATH,
+  WALLET_LINK_NEXT,
+  WALLET_LINK_STAGING,
+} from '../constants';
 
 import { Link } from './Link';
 
-export function DownloadWalletPreview({ staging }: { staging?: boolean }) {
+export function DownloadWalletPreview() {
+  const currentDomainUrl = useContext(DomainContext);
+
+  const isPreviewEnvironment =
+    !!currentDomainUrl?.includes(WALLET_LINK_STAGING) ||
+    !!currentDomainUrl?.includes(WALLET_LINK_NEXT);
+
+  const href = isPreviewEnvironment
+    ? WALLET_DOWNLOAD_PATH
+    : WALLET_LINK_STAGING;
+
   return (
-    <Link href={staging ? WALLET_LINK_STAGING : WALLET_LINK_NEXT}>
-      FuelWallet
-    </Link>
+    <Link href={href}>FuelWallet{isPreviewEnvironment ? ' Zip' : ''}</Link>
   );
 }
