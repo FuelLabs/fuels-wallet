f4390d4004ff8efa9b3b045ef8d68b7f635a45e8
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2024-06-24 16:17:41 -0300

    revert: rename DownloadWalletZip p1

--- packages/docs/src/components/DownloadWalletPreview.tsx
+++ packages/docs/src/components/DownloadWalletPreview.tsx
@@ -2,7 +2,7 @@ import { WALLET_DOWNLOAD_PATH } from '../constants';
 import { useExtensionTitle } from '../hooks/useExtensionTitle';
 import { Link } from './Link';
 
-export function DownloadWalletPreview() {
+export function DownloadWalletZip() {
   const title = useExtensionTitle();
 
   return <Link href={WALLET_DOWNLOAD_PATH}>{`${title}  zip file`}</Link>;
