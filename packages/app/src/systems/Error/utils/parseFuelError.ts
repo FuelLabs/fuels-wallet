f0ef1b86e6a26243a62563caa23c373573bc53f6
Rodrigo Branas <rodrigo.branas@gmail.com>
2024-06-26 18:00:01 -0300

    considering only erros with componentStack as ReactError

--- packages/app/src/systems/Error/utils/parseFuelError.ts
+++ packages/app/src/systems/Error/utils/parseFuelError.ts
@@ -13,11 +13,10 @@ export function parseFuelError(error: any): FuelWalletError {
 export function createError(e: FuelWalletError): FuelWalletError | Error {
   // biome-ignore lint/suspicious/noExplicitAny: <explanation>
   let syntheticError: any = e;
-
   if (e.error && 'stack' in e.error) {
     syntheticError = new Error(e.error.message);
     syntheticError.stack = e.error.stack;
-  } else if (e.reactError) {
+  } else if (e.reactError && 'componentStack' in e.reactError) {
     syntheticError = new Error('React Error');
     syntheticError.stack = e.reactError.componentStack;
   }
