089ff20ce2d72c682d2aa788b7c2857a800c6e7e
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2024-06-26 13:52:49 -0300

    feat: deduce current env from next envs

--- packages/docs/src/hooks/useCurrentEnv.tsx
+++ packages/docs/src/hooks/useCurrentEnv.tsx
@@ -1,28 +1,5 @@
-'use client';
-import {
-  IS_PUBLIC_PREVIEW,
-  WALLET_LINK_NEXT,
-  WALLET_LINK_STAGING,
-} from '../constants';
+import { CURRENT_ENV, Environment } from '../constants';
 
-export enum Environment {
-  DEV = 'DEV',
-  PROD = 'PROD',
-  STAGING = 'STAGING',
-  PREVIEW = 'PREVIEW',
-  NEXT = 'NEXT',
-}
-
-export function useCurrentEnv() {
-  const currentDomain =
-    typeof window !== 'undefined'
-      ? `${window.location.protocol}//${window.location.hostname}`
-      : '';
-
-  if (currentDomain?.includes('http://localhost')) return Environment.DEV;
-  if (currentDomain?.includes(WALLET_LINK_NEXT)) return Environment.NEXT;
-  if (currentDomain?.includes(WALLET_LINK_STAGING)) return Environment.STAGING;
-  if (IS_PUBLIC_PREVIEW) return Environment.PREVIEW;
-
-  return Environment.PROD;
+export function useCurrentEnv(): Environment {
+  return Environment[CURRENT_ENV];
 }
