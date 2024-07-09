cfbb47154baf5da11e2306b458bc04ca92d312ae
Rodrigo Branas <rodrigo.branas@gmail.com>
2024-07-09 10:52:27 -0300

    splitting the tests into regular, fast and regular changing to fast fee

--- packages/app/playwright/e2e/SendTransaction.test.ts
+++ packages/app/playwright/e2e/SendTransaction.test.ts
@@ -123,7 +123,95 @@ test.describe('SendTransaction', () => {
     await hasText(page, 'success');
   });
 
-  test('Send transaction with custom fee', async () => {
+  test('Send transaction with regular fee', async () => {
+    const receiverWallet = Wallet.generate({
+      provider,
+    });
+    await visit(page, '/send');
+    await page.waitForSelector('[aria-disabled="true"]');
+    await getButtonByText(page, 'Select one asset').click();
+    await page.getByText('Ethereum').click();
+    await getInputByName(page, 'address').fill(receiverWallet.address.toB256());
+    await getInputByName(page, 'amount').fill('0.001');
+
+    // Selecting and extracting regular fee amount
+    const regularFeeComponent = getByAriaLabel(page, 'fee value:Regular');
+    await regularFeeComponent.click();
+    const regularFeeAmount = (await regularFeeComponent.textContent())
+      .replace(' ETH', '')
+      .trim();
+
+    await getButtonByText(page, 'Review').click();
+
+    // Extract and compare the network fee amount, checking if its equal to regular fee amont
+    const networkFeeComponentWithRegular = getByAriaLabel(
+      page,
+      'fee value:Network'
+    ).first();
+    const networkFeeAmountWithRegular = (
+      await networkFeeComponentWithRegular.textContent()
+    )
+      .replace(' ETH', '')
+      .trim();
+    // Validating the amount
+    expect(regularFeeAmount).toBe(networkFeeAmountWithRegular);
+
+    await hasText(page, /(.*)ETH/);
+
+    await getButtonByText(page, 'Approve').click();
+    await hasText(page, '0.001 ETH');
+
+    // Wait for transaction to be confirmed
+    await hasText(page, 'success');
+  });
+
+  test('Send transaction with fast fee', async () => {
+    const receiverWallet = Wallet.generate({
+      provider,
+    });
+    await visit(page, '/send');
+    await page.waitForSelector('[aria-disabled="true"]');
+    await getButtonByText(page, 'Select one asset').click();
+    await page.getByText('Ethereum').click();
+    await getInputByName(page, 'address').fill(receiverWallet.address.toB256());
+    await getInputByName(page, 'amount').fill('0.001');
+
+    //Selecting and extracting fast fee amount
+    const fastFeeComponent = getByAriaLabel(page, 'fee value:Fast');
+    await fastFeeComponent.click();
+
+    // Waiting button change to Review in order to change fee amount
+    await page.waitForSelector('button:has-text("Review")');
+
+    const fastFeeAmount = (await fastFeeComponent.textContent())
+      .replace(' ETH', '')
+      .trim();
+
+    await getButtonByText(page, 'Review').click();
+
+    // Extract and compere the network fee amount, checking if its equal to fast fee amount
+    const networkFeeComponentWithFast = getByAriaLabel(
+      page,
+      'fee value:Network'
+    ).first();
+    const networkFeeAmountWithFast = (
+      await networkFeeComponentWithFast.textContent()
+    )
+      .replace(' ETH', '')
+      .trim();
+    // Validating the amount
+    expect(fastFeeAmount).toBe(networkFeeAmountWithFast);
+
+    await hasText(page, /(.*)ETH/);
+
+    await getButtonByText(page, 'Approve').click();
+    await hasText(page, '0.001 ETH');
+
+    // Wait for transaction to be confirmed
+    await hasText(page, 'success');
+  });
+
+  test('Send transaction starting with regular and changing to fast fee', async () => {
     const receiverWallet = Wallet.generate({
       provider,
     });
@@ -162,8 +250,10 @@ test.describe('SendTransaction', () => {
     //Selecting and extracting fast fee amount
     const fastFeeComponent = getByAriaLabel(page, 'fee value:Fast');
     await fastFeeComponent.click();
-    // Tricky, need to wait a little bit in order to change the fee amount
-    await page.waitForTimeout(3000);
+
+    // Waiting button change to Review in order to change fee amount
+    await page.waitForSelector('button:has-text("Review")');
+
     const fastFeeAmount = (await fastFeeComponent.textContent())
       .replace(' ETH', '')
       .trim();
