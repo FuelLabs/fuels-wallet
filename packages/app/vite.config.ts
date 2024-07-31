5d714cdeccd8a862043e20fc447755d9ba4e24aa
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2024-07-31 12:10:52 -0300

    feat: setup sourcemap upload with vite

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,14 +1,13 @@
 // biome-ignore lint/style/useNodejsImportProtocol: <explanation>
 import { join } from 'path';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 import baseConfig from './vite-utils/vite.base.config';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  ...baseConfig,
-  plugins: [
+const plugins = [
+  [
     ...baseConfig.plugins,
     viteStaticCopy({
       targets: [
@@ -20,4 +19,23 @@ export default defineConfig({
       ],
     }),
   ],
+];
+
+if (process.env.VITE_SENTRY_AUTH) {
+  plugins.push(
+    sentryVitePlugin({
+      org: 'fuel-labs',
+      project: 'fuel-wallet',
+      authToken: process.env.VITE_SENTRY_AUTH,
+      sourcemaps: {
+        filesToDeleteAfterUpload: ['*.js.map'],
+      },
+    })
+  );
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  ...baseConfig,
+  plugins,
 });
