68ce83122743bbebeec8de21e4e17bc2253caa5f
LuizAsFight <felipebolsonigomes@gmail.com>
2024-08-24 17:15:42 -0300

    chore: re-include lock wallet

--- packages/app/playwright/crx/crx.test.ts
+++ packages/app/playwright/crx/crx.test.ts
@@ -658,11 +658,11 @@ test.describe('FuelWallet Extension', () => {
       expect(currentAccountEventResult).toEqual(null);
     });
 
-    // await test.step('Auto lock fuel wallet', async () => {
-    //   await getByAriaLabel(popupPage, 'Accounts').click();
-    //   await popupPage.waitForTimeout(65000);
-    //   await hasText(popupPage, 'Unlock your wallet to continue');
-    // });
+    await test.step('Auto lock fuel wallet', async () => {
+      await getByAriaLabel(popupPage, 'Accounts').click();
+      await popupPage.waitForTimeout(65000);
+      await hasText(popupPage, 'Unlock your wallet to continue');
+    });
   });
 });
 
