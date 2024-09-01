ff8b73839d2ca5b62825cbf0892ac2840f93322a
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2024-09-01 11:48:39 -0300

    chore: remove keep alive content script calls

--- packages/connections/src/ContentProxyConnection.ts
+++ packages/connections/src/ContentProxyConnection.ts
@@ -14,14 +14,12 @@ import { PING_TIMEOUT, RECONNECT_TIMEOUT } from './config';
 export class ContentProxyConnection {
   connection: chrome.runtime.Port;
   _tryReconnect?: NodeJS.Timeout;
-  _keepAlive?: NodeJS.Timeout;
   readonly connectorName: string;
 
   constructor(connectorName: string) {
     this.connection = this.connect();
     this.connectorName = connectorName;
     window.addEventListener(EVENT_MESSAGE, this.onMessageFromWindow);
-    this.keepAlive();
     this.onStartEvent();
   }
 
@@ -60,7 +58,6 @@ export class ContentProxyConnection {
     this.connection.onDisconnect.removeListener(this.onDisconnect);
     this.connection.disconnect();
     clearInterval(this._tryReconnect);
-    clearTimeout(this._keepAlive);
     window.removeEventListener(EVENT_MESSAGE, this.onMessageFromWindow);
   }
 
@@ -83,21 +80,6 @@ export class ContentProxyConnection {
         }
       }
     }, RECONNECT_TIMEOUT);
-  };
-
-  keepAlive = () => {
-    // Send ping message to background script
-    // If background script is not available,
-    // it will throw an error and we will try to reconnect.
-    try {
-      this.connection.postMessage({
-        target: BACKGROUND_SCRIPT_NAME,
-        type: MessageTypes.ping,
-      });
-      this._keepAlive = setTimeout(this.keepAlive, PING_TIMEOUT);
-    } catch (_err) {
-      this.onDisconnect();
-    }
   };
 
   static start(providerWallet: string) {
