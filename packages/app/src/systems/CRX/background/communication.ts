a0486cab4b08eba32a6367a806922a82793ce68d
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2024-09-05 14:37:59 -0300

    feat: recover services on errorBoundary activation

--- packages/app/src/systems/CRX/background/communication.ts
+++ packages/app/src/systems/CRX/background/communication.ts
@@ -6,50 +6,49 @@ import { db } from '~/systems/Core/utils/database';
 import { BackgroundService } from './services/BackgroundService';
 import { DatabaseEvents } from './services/DatabaseEvents';
 import { VaultService } from './services/VaultService';
+let backgroundService: BackgroundService | null = null;
+let vaultService: VaultService | null = null;
+let databaseEvents: DatabaseEvents | null = null;
+
+function onConnectHandler(port: chrome.runtime.Port) {
+  // Only allow connections from the extension
+  // This is to prevent other extensions from connecting
+  // to the background script and sending messages
+  // This is a security measure, this should never happen as
+  // manifest configuration externally_connectable is set to
+  // only allow connections from the extension itself
+  if (port.sender?.id !== chrome.runtime.id) {
+    port.disconnect();
+    return;
+  }
+  if ([BACKGROUND_SCRIPT_NAME, VAULT_SCRIPT_NAME].includes(port.name)) {
+    communicationProtocol.addConnection(port);
+  }
+}
+
+function onSuspendHandler() {
+  // Handle cleanup before the background script is suspended
+  backgroundService?.stop();
+  vaultService?.stop();
+  databaseEvents?.stop();
+  backgroundService = null;
+  vaultService = null;
+  databaseEvents = null;
+  db.close();
+}
+function onStartupHandler() {
+  // Handle initialization when the background script starts up
+  backgroundService = BackgroundService.start(communicationProtocol);
+  vaultService = VaultService.start(communicationProtocol);
+  databaseEvents = DatabaseEvents.start(communicationProtocol);
+}
+
+function onRestartHandler() {
+  onSuspendHandler();
+  onStartupHandler();
+}
 
 errorBoundary(() => {
-  let backgroundService: BackgroundService | null = null;
-  let vaultService: VaultService | null = null;
-  let databaseEvents: DatabaseEvents | null = null;
-  function onConnectHandler(port: chrome.runtime.Port) {
-    // Only allow connections from the extension
-    // This is to prevent other extensions from connecting
-    // to the background script and sending messages
-    // This is a security measure, this should never happen as
-    // manifest configuration externally_connectable is set to
-    // only allow connections from the extension itself
-    if (port.sender?.id !== chrome.runtime.id) {
-      port.disconnect();
-      return;
-    }
-    if ([BACKGROUND_SCRIPT_NAME, VAULT_SCRIPT_NAME].includes(port.name)) {
-      communicationProtocol.addConnection(port);
-    }
-  }
-
-  function onSuspendHandler() {
-    // Handle cleanup before the background script is suspended
-    backgroundService?.stop();
-    vaultService?.stop();
-    databaseEvents?.stop();
-    backgroundService = null;
-    vaultService = null;
-    databaseEvents = null;
-    db.close();
-  }
-
-  function onStartupHandler() {
-    // Handle initialization when the background script starts up
-    backgroundService = BackgroundService.start(communicationProtocol);
-    vaultService = VaultService.start(communicationProtocol);
-    databaseEvents = DatabaseEvents.start(communicationProtocol);
-  }
-
-  function onRestartHandler() {
-    onSuspendHandler();
-    onStartupHandler();
-  }
-
   // Initialize services when the background script starts up
   onStartupHandler();
 
@@ -57,4 +56,4 @@ errorBoundary(() => {
   chrome.runtime.onSuspend.addListener(onSuspendHandler);
   chrome.runtime.onStartup.addListener(onStartupHandler);
   chrome.runtime.onUpdateAvailable.addListener(onRestartHandler);
-});
+}, onRestartHandler);
