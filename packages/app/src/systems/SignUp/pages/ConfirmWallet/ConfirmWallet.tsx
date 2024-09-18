4ae98410f8f190cdc347466892099d4ec2253796
Oliver Simon <oliver@blockwatch.cc>
2024-09-18 15:12:01 +0200

    rename for consistency

--- packages/app/src/systems/SignUp/pages/ConfirmWallet/ConfirmWallet.tsx
+++ packages/app/src/systems/SignUp/pages/ConfirmWallet/ConfirmWallet.tsx
@@ -14,7 +14,7 @@ export function ConfirmWallet() {
     <Layout title="Create Wallet" isPublic>
       <MnemonicWrite
         title="Confirm phrase"
-        subtitle="Enter your phrase again to ensure you wrote it down correctly."
+        subtitle="Enter your seed phrase again to ensure you wrote it down correctly."
         step={3}
         error={error || context.error}
         canProceed={!hasError}
