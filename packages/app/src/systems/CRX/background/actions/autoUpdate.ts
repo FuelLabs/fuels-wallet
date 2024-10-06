3d975e3a0a236e31e0c223787baef46f301b7b69
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2024-10-06 03:47:43 -0300

    feat: remove feature flags

--- packages/app/src/systems/CRX/background/actions/autoUpdate.ts
+++ packages/app/src/systems/CRX/background/actions/autoUpdate.ts
@@ -1,8 +1,5 @@
 import { compareVersions } from 'compare-versions';
-import { CHAIN_IDS } from 'fuels';
 import { APP_VERSION, VITE_CRX_VERSION_API, WALLET_NAME } from '~/config';
-import { AccountService } from '~/systems/Account/services/account';
-import { NetworkService } from '~/systems/Network/services/network';
 
 // Check if user has any open tab if not return false
 async function isOpen() {
@@ -14,20 +11,11 @@ async function isOpen() {
   return isOpen;
 }
 
-function getCacheParam() {
-  return `?v=${Date.now()}`;
-}
-async function fetchFeatureFlags() {
-  const featureFlags = await fetch(VITE_CRX_VERSION_API + getCacheParam())
+async function getLatestVersion() {
+  const latestVersion = await fetch(VITE_CRX_VERSION_API)
     .then((res) => res.json())
     // If fails to fetch the version return a empty object
     .catch(() => ({}));
-
-  return featureFlags;
-}
-
-async function getLatestVersion() {
-  const latestVersion = await fetchFeatureFlags();
   return latestVersion[WALLET_NAME] || APP_VERSION;
 }
 
@@ -65,50 +53,6 @@ async function reloadWallet() {
   chrome.runtime.reload();
 }
 
-async function createNetwork() {
-  console.log('[FUEL WALLET] Checking create network...');
-
-  const isOpened = await isOpen();
-  if (isOpened) return;
-
-  console.log('[FUEL WALLET] Checking current account');
-  const account = await AccountService.getCurrentAccount();
-  if (!account) return;
-
-  console.log('[FUEL WALLET] Checking feature flag');
-  const featureFlags = await fetchFeatureFlags();
-  if (!featureFlags.networkUrl) return;
-
-  console.log('[FUEL WALLET] Checking has network');
-  const existsNetwork = await NetworkService.getNetworkByChainId({
-    chainId: CHAIN_IDS.fuel.mainnet,
-  });
-  if (existsNetwork?.id) {
-    if (existsNetwork.url === featureFlags.networkUrl) {
-      console.log('[FUEL WALLET] Network url is the same, skipping');
-      chrome.alarms.clear('createNetwork');
-      return;
-    }
-
-    console.log('[FUEL WALLET] Network url is different, removing and adding');
-    await NetworkService.removeNetwork({ id: existsNetwork.id });
-  }
-
-  console.log('[FUEL WALLET] Adding network');
-  const newNetworkAdded = await NetworkService.addNetwork({
-    data: {
-      chainId: CHAIN_IDS.fuel.mainnet,
-      name: 'Ignition',
-      url: featureFlags.networkUrl,
-      explorerUrl: featureFlags.blockExplorerUrl,
-    },
-  });
-  if (!newNetworkAdded?.id) return;
-  console.log('[FUEL WALLET] Selecting network');
-  await NetworkService.selectNetwork({ id: newNetworkAdded.id });
-  chrome.alarms.clear('createNetwork');
-}
-
 // Once the app is updated reload the wallet
 chrome.runtime.onUpdateAvailable.addListener(() => reloadWallet());
 chrome.alarms.onAlarm.addListener(async (alarm) => {
@@ -119,9 +63,6 @@ chrome.alarms.onAlarm.addListener(async (alarm) => {
     case 'reloadWallet':
       reloadWallet();
       break;
-    case 'createNetwork':
-      createNetwork();
-      break;
     default:
       break;
   }
@@ -129,4 +70,3 @@ chrome.alarms.onAlarm.addListener(async (alarm) => {
 
 // Register alarms to check for updates and reload the wallet
 chrome.alarms.create('autoUpdate', { periodInMinutes: 1 });
-chrome.alarms.create('createNetwork', { periodInMinutes: 1 });
