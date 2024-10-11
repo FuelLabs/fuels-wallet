a055ccbd0708f8d78b9af670c89b02db936604de
Luc van Kampen <luc@lucemans.nl>
2024-10-11 19:55:53 +0200

    Update Fuel Extension Install Link

--- packages/docs/src/components/ExampleBox.tsx
+++ packages/docs/src/components/ExampleBox.tsx
@@ -44,7 +44,7 @@ export function ExampleBox({
   const NotDetectedOverlayDefault = (
     <Box.Stack css={styles.overlay} justify="center" align="center">
       <Heading as="h6">Wallet not detected</Heading>
-      <Link href="/docs/install">
+      <Link href="https://wallet.fuel.network/docs/install/">
         Please install {extensionName} to use this demo.
       </Link>
     </Box.Stack>
