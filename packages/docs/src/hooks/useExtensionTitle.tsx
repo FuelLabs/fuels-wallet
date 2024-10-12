f7d919905e8ebda82f9e8c33987a20d3ae128e12
LuizAsFight <felipebolsonigomes@gmail.com>
2024-10-11 22:41:49 -0300

    chore: remove text diff in docs

--- packages/docs/src/hooks/useExtensionTitle.tsx
+++
@@ -1,7 +0,0 @@
-import { useIsPreviewEnv } from './useIsPreviewEnv';
-
-export function useExtensionTitle() {
-  const isPreview = useIsPreviewEnv();
-
-  return isPreview ? 'Fuel Wallet Development' : 'Fuel Wallet';
-}
