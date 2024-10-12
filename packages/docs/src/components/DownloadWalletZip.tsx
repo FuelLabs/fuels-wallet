8b0c45bc49cdba1a0e396abab5f8b34eee3b2a0d
Luiz Gomes <8636507+LuizAsFight@users.noreply.github.com>
2024-10-11 23:58:42 -0300

    chore: fix typo (#1584)

--- packages/docs/src/components/DownloadWalletZip.tsx
+++ packages/docs/src/components/DownloadWalletZip.tsx
@@ -2,5 +2,5 @@ import { WALLET_DOWNLOAD_PATH } from '../constants';
 import { Link } from './Link';
 
 export function DownloadWalletZip() {
-  return <Link href={WALLET_DOWNLOAD_PATH}>Fuwl Wallet zip file</Link>;
+  return <Link href={WALLET_DOWNLOAD_PATH}>Fuel Wallet zip file</Link>;
 }
