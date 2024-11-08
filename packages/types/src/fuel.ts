f83da7e317921dce394f13ade7b9649ed9269944
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2024-11-08 14:36:11 +0700

    chore: remove unused typings

--- packages/types/src/fuel.ts
+++ packages/types/src/fuel.ts
@@ -50,18 +50,6 @@ export type CommunicationEventArg<T> = T extends MessageTypes.request
           ? string
           : unknown;
 
-export type DatabaseEvents = ['delete', 'create', 'update'];
-export type DatabaseObservableEvent<T extends Array<string>> =
-  `${T[number]}:${DatabaseEvents[number]}`;
-
-export type DatabaseEventArg<T extends string> = T extends `${string}:create`
-  ? ICreateChange
-  : T extends `${string}:update`
-    ? IUpdateChange
-    : T extends `${string}:delete`
-      ? IDeleteChange
-      : unknown;
-
 export type FuelProviderConfig = {
   id?: string;
   url: string;
