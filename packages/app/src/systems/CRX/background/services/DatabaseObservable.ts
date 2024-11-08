40bb91c260308db89339d1f1cf35ffd68b9c5d44
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2024-11-08 14:35:52 +0700

    feat: replace dexie observable implementation

--- packages/app/src/systems/CRX/background/services/DatabaseObservable.ts
+++ packages/app/src/systems/CRX/background/services/DatabaseObservable.ts
@@ -1,51 +1,85 @@
 // biome-ignore lint/style/useNodejsImportProtocol: <explanation>
-import EventEmitter from 'events';
-import type {
-  DatabaseEventArg,
-  DatabaseObservableEvent,
+import { EventEmitter } from 'events';
+import {
+  DatabaseChangeType,
+  type ICreateChange,
+  type IDeleteChange,
+  type IUpdateChange,
 } from '@fuel-wallet/types';
-import type { IDatabaseChange } from 'dexie-observable/api';
+import type { Dexie, IndexableType, Transaction } from 'dexie';
 import { db } from '~/systems/Core/utils/database';
 
 export class DatabaseObservable<
-  TableNames extends Array<string>,
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  TableNames extends Array<string> = any[],
 > extends EventEmitter {
   constructor() {
     super();
     // Bind methods to ensure correct `this` context
-    this.onChanges = this.onChanges.bind(this);
+    this.onCreating = this.onCreating.bind(this);
+    this.onUpdating = this.onUpdating.bind(this);
+    this.onDeleting = this.onDeleting.bind(this);
 
     this.setupListeners();
   }
 
-  onChanges(changes: Array<IDatabaseChange>) {
-    for (const change of changes) {
-      switch (change.type) {
-        case 1:
-          super.emit(`${change.table}:create`, change);
-          break;
-        case 2:
-          super.emit(`${change.table}:update`, change);
-          break;
-        case 3:
-          super.emit(`${change.table}:delete`, change);
-          break;
-        default:
-          break;
-      }
+  setupListeners() {
+    for (const table of Object.keys(db._allTables)) {
+      db.table(table).hook(
+        'creating',
+        this.onCreating(table as TableNames[number])
+      );
+      db.table(table)
+        .hook('updating')
+        .subscribe(this.onUpdating(table as TableNames[number]));
+      db.table(table).hook('deleting', this.onDeleting(table));
     }
   }
 
-  setupListeners() {
-    db.on('changes', this.onChanges);
-    db.open();
+  onCreating(table: TableNames[number]) {
+    return (primKey: unknown, obj: unknown, _transaction: Transaction) => {
+      const change: ICreateChange = {
+        type: DatabaseChangeType.Create,
+        table,
+        key: primKey,
+        obj,
+      };
+      this.emit(`${table}:create`, change);
+    };
   }
 
-  on<T extends DatabaseObservableEvent<TableNames>>(
-    eventName: T,
-    listener: (event: DatabaseEventArg<T>) => void
-  ): this {
-    return super.on(eventName, listener);
+  onUpdating(table: TableNames[number]) {
+    return (
+      mods: unknown,
+      primKey: TableNames[number],
+      obj: unknown,
+      _transaction: Transaction
+    ) => {
+      const change: IUpdateChange<TableNames[number]> = {
+        type: DatabaseChangeType.Update,
+        table,
+        key: primKey as TableNames[number],
+        mods,
+        obj,
+      };
+      this.emit(`${table}:update`, change);
+    };
+  }
+
+  onDeleting(table: TableNames[number]) {
+    return (
+      primKey: IndexableType,
+      obj: unknown,
+      _transaction: Transaction
+    ) => {
+      const change: IDeleteChange<TableNames[number]> = {
+        type: DatabaseChangeType.Delete,
+        table,
+        key: primKey as TableNames[number],
+        oldObj: obj,
+      };
+      this.emit(`${table}:delete`, change);
+    };
   }
 
   destroy() {
