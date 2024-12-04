7f98ae5b3c4e59a0958d36f0e9f5d348b7f992c0
Nelito Junior <ngvjqp@gmail.com>
2024-12-04 18:48:07 -0300

    Revert "Update playwright.config.ts"
    This reverts commit 8183d077f7aa3c35802d699caf81a59a2818402c.

--- packages/e2e-contract-tests/playwright.config.ts
+++ packages/e2e-contract-tests/playwright.config.ts
@@ -10,7 +10,7 @@ const IS_CI = process.env.CI;
 const config: PlaywrightTestConfig = defineConfig({
   testDir: './playwright',
   outputDir: './playwright-results/',
-  retries: 3,
+  retries: 1,
   maxFailures: IS_CI ? 1 : undefined,
   workers: 1,
   timeout: 60_000,
