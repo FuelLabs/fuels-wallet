23949515cf8d5c02c9320cbbbbf2d2484f66c26d
Nelito Junior <ngvjqp@gmail.com>
2024-12-09 17:29:21 -0300

    feat(playwright): adjust retries and maxFailures for CI environments

--- packages/app/playwright.crx-lock.config.ts
+++ packages/app/playwright.crx-lock.config.ts
@@ -3,8 +3,12 @@ import { join } from 'path';
 import { defineConfig } from '@playwright/test';
 import { playwrightConfig } from './playwright.config';
 
+const IS_CI = process.env.CI;
+
 export default defineConfig({
   ...playwrightConfig,
+  retries: IS_CI ? 1 : 0,
+  maxFailures: IS_CI ? 2 : undefined,
   testMatch: join(__dirname, './playwright/crx/lock.test.ts'),
   testIgnore: undefined,
 });
