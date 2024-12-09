f6ab59d7103cc299ba0c34e4ab4a876aa33bf619
Nelito Junior <ngvjqp@gmail.com>
2024-12-09 19:28:44 -0300

    feat(playwright): add Chromium project configuration to Playwright setup

--- packages/app/playwright.crx-lock.config.ts
+++ packages/app/playwright.crx-lock.config.ts
@@ -1,10 +1,18 @@
 // biome-ignore lint/style/useNodejsImportProtocol: <explanation>
 import { join } from 'path';
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 import { playwrightConfig } from './playwright.config';
 
 export default defineConfig({
   ...playwrightConfig,
   testMatch: join(__dirname, './playwright/crx/lock.test.ts'),
   testIgnore: undefined,
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chromium'],
+      },
+    },
+  ],
 });
