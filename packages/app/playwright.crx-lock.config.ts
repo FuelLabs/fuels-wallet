7eeff65ba86a14ebb3d878310ae968a9145b445f
Nelito Junior <ngvjqp@gmail.com>
2024-12-09 18:50:42 -0300

    refactor(playwright): remove CI-specific retries and maxFailures from config

--- packages/app/playwright.crx-lock.config.ts
+++ packages/app/playwright.crx-lock.config.ts
@@ -1,15 +1,10 @@
 // biome-ignore lint/style/useNodejsImportProtocol: <explanation>
 import { join } from 'path';
 import { defineConfig } from '@playwright/test';
-import './load.envs';
 import { playwrightConfig } from './playwright.config';
-
-const IS_CI = process.env.CI;
 
 export default defineConfig({
   ...playwrightConfig,
-  retries: IS_CI ? 1 : 0,
-  maxFailures: IS_CI ? 2 : undefined,
   testMatch: join(__dirname, './playwright/crx/lock.test.ts'),
   testIgnore: undefined,
 });
