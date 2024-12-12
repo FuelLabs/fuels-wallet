66ba10f75999984aa4d971413e5ba8fe5511ec1b
Nelito Junior <ngvjqp@gmail.com>
2024-12-12 16:42:55 -0300

    feat(e2e-assets): add end-to-end tests and configuration for asset validation

---
+++ packages/e2e-assets/playwright.config.ts
@@ -0,0 +1,41 @@
+// biome-ignore lint/style/useNodejsImportProtocol: <explanation>
+import { join } from 'path';
+import { defineConfig, devices } from '@playwright/test';
+// biome-ignore lint/style/useNodejsImportProtocol: <explanation>
+import './load.envs.cts';
+const PORT = process.env.PORT || 3000;
+const IS_CI = process.env.CI;
+
+export default defineConfig({
+  workers: 1,
+  retries: IS_CI ? 1 : 0,
+  testMatch: 'playwright/**/*.test.ts',
+  testDir: 'playwright/',
+  outputDir: 'playwright-results/',
+  maxFailures: IS_CI ? 2 : undefined,
+  reporter: [
+    ['list', { printSteps: true }],
+    ['html', { outputFolder: './playwright-html/' }],
+  ],
+  webServer: {
+    command: 'NODE_ENV=test pnpm dev:crx',
+    reuseExistingServer: true,
+    timeout: 20000,
+    url: `http://localhost:${PORT}`,
+  },
+  use: {
+    baseURL: `http://localhost:${PORT}/`,
+    trace: 'on-first-retry',
+    actionTimeout: 5000,
+    screenshot: 'only-on-failure',
+    headless: false,
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chromium'],
+      },
+    },
+  ],
+});
---
+++ packages/e2e-assets/playwright/commons/index.ts
@@ -0,0 +1,2 @@
+export * from './locator';
+export * from './text';
