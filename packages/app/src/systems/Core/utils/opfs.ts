4bd6e86b370d17692da72ff91c2b28cb64a21c2c
Luiz Gomes <8636507+LuizAsFight@users.noreply.github.com>
2024-12-28 15:33:35 -0300

    feat: add OPFS backup layer (#1742)
    - Closes #1743 
    - Closes FE-1231

---
+++ packages/app/src/systems/Core/utils/opfs.ts
@@ -0,0 +1,37 @@
+async function initOPFS() {
+  const root = await navigator?.storage?.getDirectory();
+  return root;
+}
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export async function saveToOPFS(data: any) {
+  if (
+    !data.accounts?.length ||
+    !data.vaults?.length ||
+    !data.networks?.length
+  ) {
+    return;
+  }
+
+  const root = await initOPFS();
+  if (!root) return;
+  console.log('saving data to opfs', data);
+  const fileHandle = await root.getFileHandle('backup.json', { create: true });
+  const writable = await fileHandle.createWritable();
+  await writable.write(JSON.stringify(data));
+  await writable.close();
+}
+
+export async function readFromOPFS() {
+  const root = await initOPFS();
+  if (!root) return;
+  try {
+    const fileHandle = await root.getFileHandle('backup.json');
+    const file = await fileHandle.getFile();
+    const text = await file.text();
+    return JSON.parse(text);
+  } catch (_) {
+    // Create empty backup file if it doesn't exist
+    return {};
+  }
+}
