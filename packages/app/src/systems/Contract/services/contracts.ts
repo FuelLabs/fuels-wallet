637aaa56e6792fabf9c83861a3bf4e767da236a5
Hélcio Franco <github@helciofranco.com>
2025-01-01 19:32:38 +0800

    feat: add a service to get contracts from database

---
+++ packages/app/src/systems/Contract/services/contracts.ts
@@ -0,0 +1,17 @@
+import type { Contract } from '@fuel-wallet/types';
+import { db } from '~/systems/Core/utils/database';
+
+export type ContractsInputs = {
+  addContracts: {
+    data: Contract[];
+  };
+};
+
+// biome-ignore lint/complexity/noStaticOnlyClass: <explanation>
+export class ContractService {
+  static async getContracts() {
+    return db.transaction('r', db.assets, async () => {
+      return db.assets.toArray();
+    });
+  }
+}
