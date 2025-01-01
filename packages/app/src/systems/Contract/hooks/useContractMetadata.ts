d6bb66377b7f1f972b8821760eddcad61c3daf5c
Hélcio Franco <github@helciofranco.com>
2025-01-01 21:21:33 +0800

    feat: add useContractMetadata

---
+++ packages/app/src/systems/Contract/hooks/useContractMetadata.ts
@@ -0,0 +1,24 @@
+import type { Contract } from '@fuel-wallet/types';
+import { Services, store } from '~/store';
+import type { ContractsMachineState } from '../machines/contractsMachine';
+
+const selectors = {
+  contract(id: string) {
+    return (state: ContractsMachineState): Contract | undefined => {
+      return state.context.contracts?.find(
+        (contract) => contract.contractId === id
+      );
+    };
+  },
+};
+
+export const useContractMetadata = (
+  id: `0x${string}`
+): Contract | undefined => {
+  const contract = store.useSelector(
+    Services.contracts,
+    selectors.contract(id)
+  );
+
+  return contract;
+};
