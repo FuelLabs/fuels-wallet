ab00c71a9e5f88575b905152e8a461a482837396
Hélcio Franco <github@helciofranco.com>
2025-01-01 20:03:29 +0800

    fix: types

--- packages/app/src/systems/Contract/services/contracts.ts
+++ packages/app/src/systems/Contract/services/contracts.ts
@@ -1,4 +1,5 @@
-import type { Contract } from '@fuel-wallet/types';
+import type { Contract, EcosystemProject } from '@fuel-wallet/types';
+import { CHAIN_IDS } from 'fuels';
 import { db } from '~/systems/Core/utils/database';
 
 export type ContractsInputs = {
@@ -9,6 +10,25 @@ export type ContractsInputs = {
 
 // biome-ignore lint/complexity/noStaticOnlyClass: <explanation>
 export class ContractService {
+  static formatContractsFromEcosystem(
+    projects: EcosystemProject[]
+  ): Contract[] {
+    return projects.flatMap((project) => {
+      if (!project.contracts?.mainnet) {
+        return [];
+      }
+
+      return project.contracts.mainnet.map((contract) => {
+        return {
+          chainId: CHAIN_IDS.fuel.mainnet,
+          contractId: contract.id,
+          name: contract.name,
+          image: project.image ?? '', // @TODO: Add url with network explorerUrl
+        };
+      });
+    });
+  }
+
   static async getContracts() {
     return db.transaction('r', db.assets, async () => {
       return db.contracts.toArray();
--- packages/types/src/contract.ts
+++ packages/types/src/contract.ts
@@ -1,5 +1,5 @@
 export interface Contract {
-  chainId: string;
+  chainId: number;
   contractId: string;
   name: string;
   image: string;
