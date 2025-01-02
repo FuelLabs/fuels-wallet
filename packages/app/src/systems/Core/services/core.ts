cc953d4f62402895970140d243a3832a374aaa46
Luiz Gomes <8636507+LuizAsFight@users.noreply.github.com>
2025-01-02 04:04:50 -0300

    fix: clean opfs backup when logout (#1748)
    - Closes #1750 
    - Closes FE-1247

--- packages/app/src/systems/Core/services/core.ts
+++ packages/app/src/systems/Core/services/core.ts
@@ -1,18 +1,26 @@
+import { IS_LOGGED_KEY } from '~/config';
+import { clearParallelDb } from '~/systems/Core/utils/databaseNoDexie';
 import { VaultService } from '~/systems/Vault';
 import { db } from '../utils/database';
+import { cleanOPFS } from '../utils/opfs';
 import { Storage } from '../utils/storage';
 import { chromeStorage } from './chromeStorage';
-import { clearParallelDb } from '~/systems/Core/utils/databaseNoDexie';
-import { IS_LOGGED_KEY } from '~/config';
 
 // biome-ignore lint/complexity/noStaticOnlyClass: <explanation>
 export class CoreService {
   static async clear() {
-    await chromeStorage.clear();
     await VaultService.clear();
     await db.clear();
-    Storage.clear();
     await clearParallelDb();
+    try {
+      // this ones can fail depending on environment
+      Storage.clear();
+      await chromeStorage.clear();
+      await cleanOPFS();
+    } catch (e) {
+      console.error(e);
+    }
+
     const reloadAfterCleanCompleted = () => {
       const isLogged = Storage.getItem(IS_LOGGED_KEY);
       if (!isLogged) {
@@ -21,6 +29,7 @@ export class CoreService {
       }
       setTimeout(() => reloadAfterCleanCompleted(), 50);
     };
+
     reloadAfterCleanCompleted();
   }
 }
