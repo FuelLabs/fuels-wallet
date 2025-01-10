b4f5f49f0fbd0000f37c677d4dfbf9ce309276c5
Nelito Junior <ngvjqp@gmail.com>
2025-01-10 20:18:55 -0300

    refactor: improve logging in checkAriaLabelsContainsText function for better debugging

--- packages/e2e-contract-tests/playwright/e2e/utils/elements.ts
+++ packages/e2e-contract-tests/playwright/e2e/utils/elements.ts
@@ -5,13 +5,19 @@ export async function checkAriaLabelsContainsText(
   ariaLabel: string,
   text: string
 ) {
+  console.log(`🔍 Checking aria-label: ${ariaLabel}`);
   const locator = walletNotificationPage.locator(`[aria-label="${ariaLabel}"]`);
+  console.log(`🔍 Locator: ${locator}`);
   const count = await locator.count();
+  console.log(`🔍 Count: ${count}`);
 
   for (let i = 0; i < count; i++) {
+    console.log(`🔍 Inner HTML: ${await locator.nth(i).innerHTML()}`);
     if (text === '') {
+      console.log('🔍 Text is empty');
       expect(locator.nth(i).innerHTML()).not.toBe('');
     } else {
+      console.log(`🔍 Text: ${text}`);
       expect((await locator.nth(i).innerHTML()).includes(text)).toBeTruthy();
     }
   }
