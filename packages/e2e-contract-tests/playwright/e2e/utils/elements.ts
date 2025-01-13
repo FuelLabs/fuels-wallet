ccb19790c6e73b0441cb6254c08db6650bbc8750
Nelito Junior <ngvjqp@gmail.com>
2025-01-13 20:08:59 -0300

    refactor: restore aria label checks in elements utility for improved validation

--- packages/e2e-contract-tests/playwright/e2e/utils/elements.ts
+++ packages/e2e-contract-tests/playwright/e2e/utils/elements.ts
@@ -1,30 +1,18 @@
-import { type Page, expect } from '@playwright/test';
+import type { Page } from '@playwright/test';
 
 export async function checkAriaLabelsContainsText(
   walletNotificationPage: Page,
   ariaLabel: string,
   text: string
 ) {
-  // Disabled as the contract Sender Name shows as unknown
-  console.log(
-    'checkAriaLabelsContainsText',
-    ariaLabel,
-    text,
-    walletNotificationPage.url()
-  );
-  return;
-  // console.log(`Check if ${ariaLabel} with text ${text} is in the page`);
-  // const locator = walletNotificationPage.locator(`[aria-label="${ariaLabel}"]`);
-  // const count = await locator.count();
+  const locator = walletNotificationPage.locator(`[aria-label="${ariaLabel}"]`);
+  const count = await locator.count();
 
-  // for (let i = 0; i < count; i++) {
-  //   if (text === '') {
-  //     expect(locator.nth(i).innerHTML()).not.toBe('');
-  //   } else {
-  //     console.log(
-  //       `Expecting ${text} to be in ${await locator.nth(i).innerHTML()}`
-  //     );
-  //     expect((await locator.nth(i).innerHTML()).includes(text)).toBeTruthy();
-  //   }
-  // }
+  for (let i = 0; i < count; i++) {
+    if (text === '') {
+      expect(locator.nth(i).innerHTML()).not.toBe('');
+    } else {
+      expect((await locator.nth(i).innerHTML()).includes(text)).toBeTruthy();
+    }
+  }
 }
