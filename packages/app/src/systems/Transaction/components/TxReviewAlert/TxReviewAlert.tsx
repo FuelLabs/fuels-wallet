a31fa8715a093bac2ac9705b485a0905d620602e
Luiz Gomes <8636507+LuizAsFight@users.noreply.github.com>
2025-03-26 23:28:57 -0300

    chore: fix typo + audit (#1887)

--- packages/app/src/systems/Transaction/components/TxReviewAlert/TxReviewAlert.tsx
+++ packages/app/src/systems/Transaction/components/TxReviewAlert/TxReviewAlert.tsx
@@ -9,7 +9,7 @@ export const TxReviewAlert = () => {
     >
       <Box.Flex css={styles.reviewTxBadge}>
         <Icon icon="InfoCircle" stroke={2} size={16} />
-        Double-check transaction details before submit.
+        Double-check transaction details before submission
       </Box.Flex>
     </Box>
   );
