bbe52918447c86de883b3d8c6cbececa218e58e0
Nelito Junior <ngvjqp@gmail.com>
2025-07-22 18:45:02 -0300

    fix: E2E Chrome beta (#1954)

---
+++ .changeset/plenty-ads-trade.md
@@ -0,0 +1,6 @@
+---
+"@fuels/playwright-utils": minor
+"fuels-wallet": minor
+---
+
+fix: disable chrome-beta tests as they don't support importing chrome extensions anymore
--- packages/app/playwright.config.ts
+++ packages/app/playwright.config.ts
@@ -40,16 +40,18 @@ export const playwrightConfig: PlaywrightTestConfig = {
   },
   projects: [
     {
+      // For Google Chrome, Microsoft Edge and other Chromium-based browsers, by default,
+      // Playwright uses open source Chromium builds.
+      // Since the Chromium project is ahead of the branded browsers,
+      // when the world is on Google Chrome N, Playwright already supports Chromium N+1 t
+      // hat will be released in Google Chrome and Microsoft Edge a few weeks later.
       name: 'chromium',
       use: {
-        ...devices['Desktop Chromium'],
-      },
-    },
-    {
-      name: 'chrome-beta',
-      use: {
-        channel: 'chrome-beta',
+        channel: 'chromium',
         ...devices['Desktop Chrome'],
+        launchOptions: {
+          args: ['--disable-features=ExtensionDisableUnsupportedDeveloper'],
+        },
       },
     },
   ],
